8b458d6b0d761430aa6b5fdd6ff6b4c8919ba634
gregr <greg.weiqi@gmail.com>
2015-09-11 21:32:00 -0400

    Define term, value, and subst annotations

--- semantics/term.rkt
+++ semantics/term.rkt
@@ -22,12 +22,14 @@
   )
 
 (records term
+  (t-ann ann t)
   (t-subst  s t)
   (t-value  v)
   (t-unpair bit pair)
   (t-apply  proc arg))
 
 (records value
+  (v-ann ann v)
   (v-unit)
   (v-bit  b)
   (v-pair l r)
@@ -36,4 +38,5 @@
 
 (records bit (b-0) (b-1))
 
+(record s-ann ann v)
 (record subst bindings lift)
