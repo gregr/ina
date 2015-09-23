e3c025b4b7d82dd5470ae76046603dd0c238cf77
gregr <greg.weiqi@gmail.com>
2015-09-23 13:00:09 -0400

    Support default env argument to result of denote

--- bootstrap/denotation.rkt
+++ bootstrap/denotation.rkt
@@ -19,14 +19,14 @@
 (define (env-add env v) (cons v env))
 (define (env-extend env vs) (append vs env))
 
-(define (denote tm (annotate ~a) (scope 0) (path '()))
-  (define (denote-value val scope path)
+(define (pre-denote tm annotate scope path)
+  (define (pre-denote-value val scope path)
     (match val
-      ((v-lam body) (let ((db (denote-term
+      ((v-lam body) (let ((db (pre-denote-term
                                 body (+ 1 scope) (list* 'body path))))
                       (lambda (env) (lambda (arg) (db (env-add env arg))))))
-      ((v-pair l r) (let ((dl (denote-value l scope (list* 'l path)))
-                          (dr (denote-value r scope (list* 'r path))))
+      ((v-pair l r) (let ((dl (pre-denote-value l scope (list* 'l path)))
+                          (dr (pre-denote-value r scope (list* 'r path))))
                       (lambda (env) (cons (dl env) (dr env)))))
       ((v-bit bt)   (match bt ((b-0) (lambda (env) 0))
                               ((b-1) (lambda (env) 1))))
@@ -34,25 +34,25 @@
       ((v-var idx)  (if (< idx scope) (lambda (env) (env-ref env idx))
                       (error (format "unbound variable; depth=~a index=~a; ~a"
                                      scope idx (annotate path)))))))
-  (define (denote-term tm scope path)
+  (define (pre-denote-term tm scope path)
     (match tm
       ((t-subst (subst bindings lift) tm)
        (lets (values dbindings _) =
              (forf result = '() sub-path = (list* 'first path)
                    val <- bindings
-                   (values (list* (denote-value
+                   (values (list* (pre-denote-value
                                     val scope (list* 's 'bindings sub-path))
                                   result)
                            (list* 'rest sub-path)))
-             dbody = (denote-term tm (+ (length bindings) scope)
+             dbody = (pre-denote-term tm (+ (length bindings) scope)
                                   (list* 't path))
              (lambda (env)
                (dbody (env-extend env (reverse (forl dval <- dbindings
                                                      (dval env))))))))
-      ((t-value val)    (denote-value val scope (list* 'v path)))
+      ((t-value val)    (pre-denote-value val scope (list* 'v path)))
       ((t-unpair tbit tpair)
-       (let ((dbit (denote-term tbit scope (list* 'bit path)))
-             (dpair (denote-term tpair scope (list* 'pair path))))
+       (let ((dbit (pre-denote-term tbit scope (list* 'bit path)))
+             (dpair (pre-denote-term tpair scope (list* 'pair path))))
          (lambda (env)
            (match (dpair env)
              ((cons pl pr)
@@ -63,20 +63,22 @@
              (val (error (format "cannot unpair non-pair ~v; depth=~a; ~a"
                                  val scope (annotate path))))))))
       ((t-apply tproc targ)
-       (let ((dproc (denote-term tproc scope (list* 'proc path)))
-             (darg (denote-term targ scope (list* 'arg path))))
+       (let ((dproc (pre-denote-term tproc scope (list* 'proc path)))
+             (darg (pre-denote-term targ scope (list* 'arg path))))
          (lambda (env)
            (match (dproc env)
              ((? procedure? proc) (proc (darg env)))
              (val (error (format "cannot apply non-procedure ~v; depth=~a; ~a"
                                  val scope (annotate path))))))))))
-  (denote-term tm scope path))
+  (pre-denote-term tm scope path))
 
+(define ((denote term (annotate ~a)) (env env-empty))
+  ((pre-denote term annotate 0 '()) env))
 
 (module+ test
-  (define dt (denote (t-value (v-pair
-                                (v-var 0) (v-pair (v-bit (b-0)) (v-unit))))
-                     ~a 1))
+  (define dt
+    (pre-denote (t-value (v-pair (v-var 0) (v-pair (v-bit (b-0)) (v-unit))))
+                ~a 1 '()))
   (check-equal? (dt (env-add env-empty 'a))
                 '(a 0)))
 
@@ -94,6 +96,6 @@
       (t-value (v-var 0))))
   (define test-term-1
     (t-apply (t-value (v-lam test-term-0)) (t-value (v-bit (b-1)))))
-  (define completed ((denote test-term-1) env-empty))
-  (check-equal? ((denote (just-x (step test-term-1))) env-empty) completed)
+  (define completed ((denote test-term-1)))
+  (check-equal? ((denote (just-x (step test-term-1)))) completed)
   (check-equal? completed '(1 . 1)))
