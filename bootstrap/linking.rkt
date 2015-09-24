2c19d8ccc5956f6bb6c4bf006e36d6d5ed703034
gregr <greg.weiqi@gmail.com>
2015-09-24 13:37:55 -0400

    Add bootstrap/linking.rkt

---
+++ bootstrap/linking.rkt
@@ -0,0 +1,45 @@
+#lang racket/base
+(provide
+  tuple0->list
+  open-module
+  link-module
+  link-program
+  )
+
+(require
+  "operation.rkt"
+  "parsing.rkt"
+  "substitution.rkt"
+  "term.rkt"
+  gregr-misc/list
+  gregr-misc/sugar
+  racket/match
+  )
+
+(module+ test
+  (require rackunit))
+
+(define (tuple0->list tup)
+  (match tup
+    ((v-unit)     '())
+    ((v-pair l r) (list* l (tuple0->list r)))))
+
+(define (open-module mod specs)
+  (forl spec <- specs
+        (match spec
+          ((? symbol?)               (list spec (mod spec)))
+          ((list name original-name) (list name (mod original-name)))
+          (_ (error (format "invalid open-module spec: ~a" spec))))))
+
+(def ((link-program parse) imports body)
+  (list import-params import-args) = (zip-default '(() ()) imports)
+  (if (null? import-params) (parse body)
+    (build-apply (parse `(lambda ,import-params ,body)) import-args)))
+
+; TODO: bindings must not redefine 'pair'; this can be fixed with more work
+(def ((link-module parse) imports bindings)
+  names = (append (map car imports) (map car bindings))
+  body = (foldr (lambda (name acc) `(pair ,name ,acc)) '() names)
+  prog = ((link-program parse) imports `(let* ,bindings ,body))
+  vals = (tuple0->list (t-value-v (substitute-full (step-complete prog))))
+  (make-immutable-hash (zip-with* cons names vals)))
