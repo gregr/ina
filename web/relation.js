d7913936f0929c319c39b779a5b038925ad772f9
gregr <greg.weiqi@gmail.com>
2016-09-14 06:03:26 -0400

    Add relation.js

---
+++ web/relation.js
@@ -0,0 +1,53 @@
+/*
+
+* relation definition
+** extensional
+*** defined by data
+** intensional
+*** defined by computation
+*** potentially infinite modes
+*** decidable modes may be precomputed
+*** any mode may be memoized
+
+* relation properties
+** decidable
+*** finite number of answers
+*** may require tabling/memoization to terminate when calculating fixed points
+** semi-deterministic
+*** 0 or 1 answer
+
+* negation
+** allowed for any decidable goal
+
+* constraints
+** should be idempotent, unlike CHR
+** lattice-based (a bit ad-hoc at the moment)
+*** types (possibly other predicates?)
+*** intervals
+*** finite domains
+*** (in)equalities
+*** computations (e.g. arithmetic: a + b = c)
+*** negated versions of the above
+
+* stratification and analyses?
+
+* higher level sugar
+** multi-site definitions/assignments
+*** definitions may be distributed across several source locations
+** monotonic/merging assignment
+*** merge operations must be commutative, associative, idempotent (CRDTs)
+*** generalizes relation definition?
+** perturbance-based assignment
+*** optionally deferred
+*** optionally pinned
+**** pinned values are not changeable by future perturbances until unpinned
+** conditionals
+*** optionally multi-arm
+*** optionally event/signal-based
+** arithmetic (in)equalities
+*** could be generalized to other structures/computations
+**** e.g. (append a b) == (append c d)
+** aggregation
+** higher-order functional orchestration and communication
+
+*/
