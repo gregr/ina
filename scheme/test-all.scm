bd5512643781ddfa3e4568d900d56f5cb745765d
gregr <greg.weiqi@gmail.com>
2017-06-05 09:38:38 -0400

    Add test-all.scm

---
+++ scheme/test-all.scm
@@ -0,0 +1,11 @@
+(printf "\nNaive evaluate\n")
+(load "eval-naive.scm")
+(load "test.scm")
+
+(printf "\nHigher order evaluate\n")
+(load "eval-ho.scm")
+(load "test.scm")
+
+(printf "\nFirst order evaluate\n")
+(load "eval-fo.scm")
+(load "test.scm")
