28730326b10382a58e6f3dc22a8202166b1a9dc1
gregr <greg.weiqi@gmail.com>
2017-06-05 15:03:01 -0400

    Rename naive evaluate to denote

--- scheme/eval-naive.scm
+++ scheme/eval-naive.scm
@@ -10,34 +10,34 @@
 ;; flatten environment representation, strip/gc closure environments
 (load "common.scm")
 
-(define (evaluate-procedure body params env)
-  (lambda args (evaluate body (env-extend* env params args))))
-(define (evaluate-application proc args env)
-  (apply proc (map (lambda (arg) (evaluate arg env)) args)))
+(define (denote-procedure body params env)
+  (lambda args (denote body (env-extend* env params args))))
+(define (denote-application proc args env)
+  (apply proc (map (lambda (arg) (denote arg env)) args)))
 
-(define (evaluate expr env)
+(define (denote expr env)
   (cond
     ((or (boolean? expr) (number? expr)) expr)
     ((symbol? expr) (env-lookup env expr))
     ((pair? expr)
      (let ((head (car expr)))
        (if (or (not (symbol? head)) (bound? env head))
-         (evaluate-application (evaluate head env) (cdr expr) env)
+         (denote-application (denote head env) (cdr expr) env)
          (case head
            ((quote) (car (syntax-pattern '(quote #f) expr)))
            ((lambda)
             (let* ((parts (syntax-pattern '(lambda #f #f) expr))
                    (params (car parts))
                    (body (cadr parts)))
-              (evaluate-procedure body params env)))
+              (denote-procedure body params env)))
            ((if)
             (let* ((parts (syntax-pattern '(if #f #f #f) expr))
                    (condition (car parts))
                    (alt-true (cadr parts))
                    (alt-false (caddr parts)))
-              (if (evaluate condition env)
-                (evaluate alt-true env)
-                (evaluate alt-false env))))
+              (if (denote condition env)
+                (denote alt-true env)
+                (denote alt-false env))))
            ((let)  ;; TODO: optionally-named.
             (let* ((parts (syntax-pattern '(let #f #f) expr))
                    (bindings (map (lambda (b) (syntax-pattern '(#f #f) b))
@@ -45,8 +45,8 @@
                    (body (cadr parts))
                    (params (map car bindings))
                    (args (map cadr bindings)))
-              (evaluate-application
-                (evaluate-procedure body params env) args env)))
+              (denote-application
+                (denote-procedure body params env) args env)))
            ((quasiquote)
             (let loop ((level 0) (qq-expr (car (syntax-pattern
                                                  '(quasiquote #f) expr))))
@@ -55,25 +55,25 @@
                 `(((,'unquote #f)
                    ,(lambda (datum)
                       (if (= 0 level)
-                        (evaluate datum env)
+                        (denote datum env)
                         `(,'unquote ,(loop (- level 1) datum)))))
                   ((quasiquote #f)
                    ,(lambda (datum)
                       `(,'quasiquote ,(loop (+ level 1) datum))))
                   (,'unquote
-                    ,(lambda _ (error 'evaluate-quasiquote
+                    ,(lambda _ (error 'denote-quasiquote
                                       (format "bad unquote ~s" expr))))
                   ((,'unquote)
-                   ,(lambda _ (error 'evaluate-quasiquote
+                   ,(lambda _ (error 'denote-quasiquote
                                      (format "bad unquote ~s" expr))))
                   ((,'unquote #f #f . #f)
-                   ,(lambda _ (error 'evaluate-quasiquote
+                   ,(lambda _ (error 'denote-quasiquote
                                      (format "bad unquote ~s" expr))))
                   ((#f . #f)
                    ,(lambda (a d) `(,(loop level a) . ,(loop level d))))
                   (#f ,id)))))
-           (else (error 'evaluate (format "unbound variable ~s" head)))))))
-    (else (error 'evaluate (format "invalid syntax ~s" expr)))))
+           (else (error 'denote (format "unbound variable ~s" head)))))))
+    (else (error 'denote (format "invalid syntax ~s" expr)))))
 
 (define env-initial
   `((cons . ,cons)
@@ -87,3 +87,5 @@
     (procedure? . ,procedure?)
     (apply . ,apply)
     . ,env-empty))
+
+(define (evaluate expr env) (denote expr env))
