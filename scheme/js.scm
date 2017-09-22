822fe70229daba5187818ecc9f0bb55a03d62cf4
gregr <greg.weiqi@gmail.com>
2017-09-22 16:15:52 -0400

    Begin defining js-stmt

--- scheme/js.scm
+++ scheme/js.scm
@@ -1,8 +1,3 @@
-;; Value transfer
-;; declare: var
-;; Control transfer
-;; apply, label: {}, if/else, switch/case, [label:] for, for/in, [label:] while, do/while, break [any label], continue [loop label], return, throw, try/catch
-
 ;; Misc
 ;; publicly visible (global?) names (and symbol->name table for linking)
 ;; bind used globals to local variables
@@ -13,6 +8,76 @@
 ;; JSON subset
 ;; Node.js has global,require instead of window
 
+;; TODO: support weakly-unique object keys.
+
+(define (js-stmt* ctx stx*) (map (lambda (stx) (js-stmt ctx stx)) stx*))
+(define (js-stmt ctx stx)
+  ;; TODO: need to produce an updated ctx.
+  (match stx
+    (`(vars ,(? list? binding*))
+      (string-append "var " (comma* (js-binding* ctx binding*)) ";"))
+    (`(return ,expr) (string-append "return " (js-expr ctx expr) ";"))
+    (`(throw ,expr) (string-append "throw " (js-expr ctx expr) ";"))
+    (`(break ,label) (string-append "break " (js-label ctx label) ";"))
+    (`(continue ,label) (string-append "continue " (js-label ctx label) ";"))
+    (`(if/else ,test ,(? list? true*) ,(? list? false*))
+      (string-append "if(" (js-expr ctx test) "){"
+                     (adjacent* (js-stmt* ctx true*)) "}else{"
+                     (adjacent* (js-stmt* ctx false*)) "}"))
+    (`(for ,pre ,test ,iter ,(? list? stmt*))
+      ;; TODO: update ctx with pre.
+      (string-append "for("(js-stmt ctx pre) ";" (js-expr ctx test) ";"
+                     (js-expr ctx iter) "){"
+                     (adjacent* (js-stmt* ctx stmt*)) "}"))
+    (`(for/in ,(? symbol? iname) ,obj ,(? list? stmt*))
+      ;; TODO: update ctx with iname.
+      (string-append "for(var " (js-ref ctx iname) " in " (js-expr ctx obj)
+                     "){" (adjacent* (js-stmt* ctx stmt*)) "}"))
+    (`(while ,test ,(? list? stmt*))
+      (string-append "while(" (js-expr ctx test) "){"
+                     (adjacent* (js-stmt* ctx stmt*)) "}"))
+    (`(do/while ,(? list? stmt*) ,test)
+      (string-append "do{" (adjacent* (js-stmt* ctx stmt*))
+                     "}while(" (js-expr ctx test) ");"))
+    (`(switch ,scrutinee ,(? list? case*))
+      (string-append "switch(" (js-expr ctx scrutinee) "){"
+                     (adjacent* (js-case* ctx case*)) "}"))
+    (`(try/catch/finally ,(? list? try*) ,(? symbol? ename)
+                         ,(? list? catch*) ,(? list? finally*))
+      ;; TODO: update ctx with ename.
+      (string-append "try{" (adjacent* (js-stmt* ctx try*))
+                     "}catch(" (js-ref ctx ename) "){"
+                     (adjacent* (js-stmt* ctx catch*)) "}finally{"
+                     (adjacent* (js-stmt* ctx finally*)) "}"))
+    (`(labeled ,(and (not #f) label) stmt)
+      ;; TODO: incorporate label into ctx.
+      (string-append (js-label ctx label) ":" (js-stmt ctx stmt)))
+    (`(block ,(? list? stmt*))
+      (string-append "{;" (adjacent* (js-stmt* ctx stmt*)) "}"))
+    (_ (string-append (js-expr ctx stx) ";"))))
+
+(define (js-binding* ctx binding*)
+  (map (lambda (b) (js-binding ctx b)) binding*))
+(define (js-binding ctx binding)
+  ;; TODO: update ctx.
+  (match binding
+    ((? symbol?) (js-var ctx binding))
+    (`(,(? symbol? name) ,rhs)
+      (string-append (js-var ctx name) "=" (js-expr ctx rhs)))))
+
+(define (js-case* ctx case*) (map (lambda (cstx) (js-case ctx cstx)) case*))
+(define (js-case ctx case-stx)
+  (match case-stx
+    (`(case ,val ,(? list? stmt*))
+      (string-append "case " (js-expr ctx val) ":{"
+                     (adjacent* (js-stmt* ctx stmt*)) "}"))
+    (`(default ,(? list? stmt*))
+      (string-append "default:{" (adjacent* (js-stmt* ctx stmt*)) "}"))))
+
+;; TODO: refer to label table.
+(define (js-label ctx label) (if label (symbol->string label) ""))
+
+(define (adjacent* s*) (string-join s* ""))
 (define (comma* s*) (string-join s* ","))
 
 ;; TODO: refer to global symbol table.
@@ -43,6 +108,9 @@
 (define (js-put op ctx lhs rhs)
   (string-append "(" (js-ref ctx lhs) op (js-expr ctx rhs) ")"))
 
+;; TODO: manage ctx.
+(define (sym->ident* ctx param*) (map symbol->string param*))
+
 (define (js-expr* ctx stx*) (map (lambda (stx) (js-expr ctx stx)) stx*))
 (define (js-expr ctx stx)
   ;; TODO: regular expressions?
@@ -58,8 +126,8 @@
     (`(object ,(? list? kv*))
       (string-append "{" (comma* (js-kv* ctx kv*)) "}"))
     (`(function ,(? list? param*) ,(? list? body*))
-      (string-append "function(" (comma* (sym->ident* param*))
-                     "){\"use strict\";" (js-stmt* ctx body*) "}"))
+      (string-append "function(" (comma* (sym->ident* ctx param*))
+                     "){\"use strict\";" (adjacent* (js-stmt* ctx body*)) "}"))
     (`(app ,rator ,(? list? rand*))
       (string-append
         (js-expr ctx rator) "(" (comma* (js-expr* ctx rand*)) ")"))
