f43aef1f1ef2aa377b7d0b1fd1d4bd1c418ddb2f
gregr <greg.weiqi@gmail.com>
2017-09-22 14:04:36 -0400

    Create and sketch js.scm

---
+++ scheme/js.scm
@@ -0,0 +1,38 @@
+;; Values
+;; number
+;; boolean
+;; string
+;; array
+;; object
+;; null
+;; undefined
+;; function
+;; regexp
+
+;; Primitive operations
+;; typeof, instanceof, in
+;; arithmetic: +, -, *, /, %, -(unary)
+;; bitwise: &, |, ^, ~, <<. >>, >>>
+;; logical: !, ===, !==, ==, !=, >, <, >=, <=, &&, ||
+
+;; Value transfer
+;; declare, assign, reference: var, =, [name]
+;; get, put: [], []=, ., .=
+;; delete
+;; ? :
+;; +=, -=, *=, /=, %=, ++, --
+;; ,
+
+;; Control transfer
+;; apply, label: {}, if/else, switch/case, [label:] for, for/in, [label:] while, do/while, break [any label], continue [loop label], return, throw, try/catch
+
+;; Misc
+;; publicly visible (global?) names (and symbol->name table for linking)
+;; bind used globals to local variables
+;; inline single-use temp vars?
+;; "use strict";
+;; debugger;
+;; new, this, call/apply
+;; minification
+;; JSON subset
+;; Node.js has global,require instead of window
