bec2b43342d992315c34cd26e360a2b191d6d103
gregr <greg.weiqi@gmail.com>
2017-09-24 13:58:42 -0400

    Remove context

--- scheme/js.scm
+++ scheme/js.scm
@@ -1,119 +1,97 @@
-;; Misc
-;; publicly visible (global?) names (and symbol->name table for linking)
-;; bind used globals to local variables
-;; inline single-use temp vars?
-;; debugger;
-;; new, this, call/apply
-;; minification
-;; JSON subset
-;; Node.js has global,require instead of window
+;; TODO: JSON subset
 
-;; TODO: support weakly-unique object keys.
-
-(define (js-stmt* ctx stx*) (map (lambda (stx) (js-stmt ctx stx)) stx*))
-(define (js-stmt ctx stx)
-  ;; TODO: need to produce an updated ctx.
+(define (js-stmt* stx*) (map js-stmt stx*))
+(define (js-stmt stx)
   (match stx
     (`(vars ,(? list? binding*))
-      (string-append "var " (comma* (js-binding* ctx binding*)) ";"))
-    (`(return ,expr) (string-append "return " (js-expr ctx expr) ";"))
-    (`(throw ,expr) (string-append "throw " (js-expr ctx expr) ";"))
-    (`(break ,label) (string-append "break " (js-label ctx label) ";"))
-    (`(continue ,label) (string-append "continue " (js-label ctx label) ";"))
+      (string-append "var " (comma* (js-binding* binding*)) ";"))
+    (`(return ,expr) (string-append "return " (js-expr expr) ";"))
+    (`(throw ,expr) (string-append "throw " (js-expr expr) ";"))
+    (`(break ,label) (string-append "break " (js-label label) ";"))
+    (`(continue ,label) (string-append "continue " (js-label label) ";"))
     (`(if/else ,test ,(? list? true*) ,(? list? false*))
-      (string-append "if(" (js-expr ctx test) "){"
-                     (adjacent* (js-stmt* ctx true*)) "}else{"
-                     (adjacent* (js-stmt* ctx false*)) "}"))
+      (string-append "if(" (js-expr test) "){"
+                     (adjacent* (js-stmt* true*)) "}else{"
+                     (adjacent* (js-stmt* false*)) "}"))
     (`(for ,pre ,test ,iter ,(? list? stmt*))
-      ;; TODO: update ctx with pre.
-      (string-append "for("(js-stmt ctx pre) ";" (js-expr ctx test) ";"
-                     (js-expr ctx iter) "){"
-                     (adjacent* (js-stmt* ctx stmt*)) "}"))
+      (string-append "for("(js-stmt pre) ";" (js-expr test) ";"
+                     (js-expr iter) "){" (adjacent* (js-stmt* stmt*)) "}"))
     (`(for/in ,(? symbol? iname) ,obj ,(? list? stmt*))
-      ;; TODO: update ctx with iname.
-      (string-append "for(var " (js-ref ctx iname) " in " (js-expr ctx obj)
-                     "){" (adjacent* (js-stmt* ctx stmt*)) "}"))
+      (string-append "for(var " (js-ref iname) " in " (js-expr obj)
+                     "){" (adjacent* (js-stmt* stmt*)) "}"))
     (`(while ,test ,(? list? stmt*))
-      (string-append "while(" (js-expr ctx test) "){"
-                     (adjacent* (js-stmt* ctx stmt*)) "}"))
+      (string-append "while(" (js-expr test) "){"
+                     (adjacent* (js-stmt* stmt*)) "}"))
     (`(do/while ,(? list? stmt*) ,test)
-      (string-append "do{" (adjacent* (js-stmt* ctx stmt*))
-                     "}while(" (js-expr ctx test) ");"))
+      (string-append "do{" (adjacent* (js-stmt* stmt*))
+                     "}while(" (js-expr test) ");"))
     (`(switch ,scrutinee ,(? list? case*))
-      (string-append "switch(" (js-expr ctx scrutinee) "){"
-                     (adjacent* (js-case* ctx case*)) "}"))
+      (string-append "switch(" (js-expr scrutinee) "){"
+                     (adjacent* (js-case* case*)) "}"))
     (`(try/catch/finally ,(? list? try*) ,(? symbol? ename)
                          ,(? list? catch*) ,(? list? finally*))
-      ;; TODO: update ctx with ename.
-      (string-append "try{" (adjacent* (js-stmt* ctx try*))
-                     "}catch(" (js-ref ctx ename) "){"
-                     (adjacent* (js-stmt* ctx catch*)) "}finally{"
-                     (adjacent* (js-stmt* ctx finally*)) "}"))
+      (string-append "try{" (adjacent* (js-stmt* try*))
+                     "}catch(" (js-ref ename) "){"
+                     (adjacent* (js-stmt* catch*)) "}finally{"
+                     (adjacent* (js-stmt* finally*)) "}"))
     (`(labeled ,(and (not #f) label) stmt)
-      ;; TODO: incorporate label into ctx.
-      (string-append (js-label ctx label) ":" (js-stmt ctx stmt)))
+      (string-append (js-label label) ":" (js-stmt stmt)))
     (`(block ,(? list? stmt*))
-      (string-append "{;" (adjacent* (js-stmt* ctx stmt*)) "}"))
-    (_ (string-append (js-expr ctx stx) ";"))))
+      (string-append "{;" (adjacent* (js-stmt* stmt*)) "}"))
+    (_ (string-append (js-expr stx) ";"))))
 
-(define (js-binding* ctx binding*)
-  (map (lambda (b) (js-binding ctx b)) binding*))
-(define (js-binding ctx binding)
-  ;; TODO: update ctx.
+(define (js-binding* binding*) (map js-binding binding*))
+(define (js-binding binding)
   (match binding
-    ((? symbol?) (js-var ctx binding))
+    ((? symbol?) (js-var binding))
     (`(,(? symbol? name) ,rhs)
-      (string-append (js-var ctx name) "=" (js-expr ctx rhs)))))
+      (string-append (js-var name) "=" (js-expr rhs)))))
 
-(define (js-case* ctx case*) (map (lambda (cstx) (js-case ctx cstx)) case*))
-(define (js-case ctx case-stx)
+(define (js-case* case*) (map (lambda (cstx) (js-case cstx)) case*))
+(define (js-case case-stx)
   (match case-stx
     (`(case ,val ,(? list? stmt*))
-      (string-append "case " (js-expr ctx val) ":{"
-                     (adjacent* (js-stmt* ctx stmt*)) "}"))
+      (string-append "case " (js-expr val) ":{"
+                     (adjacent* (js-stmt* stmt*)) "}"))
     (`(default ,(? list? stmt*))
-      (string-append "default:{" (adjacent* (js-stmt* ctx stmt*)) "}"))))
+      (string-append "default:{" (adjacent* (js-stmt* stmt*)) "}"))))
 
-;; TODO: refer to label table.
-(define (js-label ctx label) (if label (symbol->string label) ""))
+(define (js-label label) (if label (symbol->string label) ""))
 
 (define (adjacent* s*) (string-join s* ""))
 (define (comma* s*) (string-join s* ","))
 
-;; TODO: refer to global symbol table.
-(define (js-var ctx stx) (symbol->string stx))
+(define (js-var stx) (symbol->string stx))
 ;; TODO: convert or validate double.
 (define (js-number stx) (number->string stx))
 ;; TODO: escape properly.
 (define (js-string stx) (string-append "\"" stx "\""))
 
-(define (js-kv* ctx kv*) (map (lambda (kv) (js-kv ctx kv)) kv*))
-(define (js-kv ctx kv)
-  ;; TODO: indicate arbitrary field names using symbols.
+(define (js-kv* kv*) (map (lambda (kv) (js-kv kv)) kv*))
+(define (js-kv kv)
   (match kv
     (`(,(? string? key) ,value)
-      (string-append (js-string key) ":" (js-expr ctx value)))))
+      (string-append (js-string key) ":" (js-expr value)))))
 
-(define (js-unop ctx name expr)
-  (string-append "(" name (js-expr ctx expr) ")"))
-(define (js-binop ctx name ea eb)
-  (string-append "(" (js-expr ctx ea) name (js-expr ctx eb) ")"))
+(define (js-unop name expr)
+  (string-append "(" name (js-expr expr) ")"))
+(define (js-binop name ea eb)
+  (string-append "(" (js-expr ea) name (js-expr eb) ")"))
 
-(define (js-ref/lhs lhs ctx stx)
+(define (js-ref/lhs lhs stx)
   (match stx
-    ((? symbol?) (js-var ctx stx))
+    ((? symbol?) (js-var stx))
     (`(index ,ref ,index)
-      (string-append (lhs ctx ref) "[" (js-expr ctx index) "]"))))
-(define (js-ref ctx stx) (js-ref/lhs js-ref ctx stx))
-(define (js-put op ctx lhs rhs)
-  (string-append "(" (js-ref ctx lhs) op (js-expr ctx rhs) ")"))
+      (string-append (lhs ref) "[" (js-expr index) "]"))))
+(define (js-ref stx) (js-ref/lhs js-ref stx))
+(define (js-put op lhs rhs)
+  (string-append "(" (js-ref lhs) op (js-expr rhs) ")"))
 
-;; TODO: manage ctx.
-(define (sym->ident* ctx param*) (map symbol->string param*))
+(define (sym->ident* param*) (map symbol->string param*))
 
-(define (js-expr* ctx stx*) (map (lambda (stx) (js-expr ctx stx)) stx*))
-(define (js-expr ctx stx)
-  ;; TODO: regular expressions?
+(define (js-expr* stx*) (map (lambda (stx) (js-expr stx)) stx*))
+(define (js-expr stx)
+  ;; TODO: support regular expressions, debugger, this, new.
   (match stx
     ((? number?) (js-number stx))
     ((? string?) (js-string stx))
@@ -122,56 +100,53 @@
     ('null "null")
     ('undefined "undefined")
     (`(array ,(? list? element*))
-      (string-append "[" (comma* (js-expr* ctx element*)) "]"))
+      (string-append "[" (comma* (js-expr* element*)) "]"))
     (`(object ,(? list? kv*))
-      (string-append "{" (comma* (js-kv* ctx kv*)) "}"))
+      (string-append "{" (comma* (js-kv* kv*)) "}"))
     (`(function ,(? list? param*) ,(? list? body*))
-      (string-append "function(" (comma* (sym->ident* ctx param*))
-                     "){\"use strict\";" (adjacent* (js-stmt* ctx body*)) "}"))
+      (string-append "function(" (comma* (sym->ident* param*))
+                     "){\"use strict\";" (adjacent* (js-stmt* body*)) "}"))
     (`(app ,rator ,(? list? rand*))
-      (string-append
-        (js-expr ctx rator) "(" (comma* (js-expr* ctx rand*)) ")"))
+      (string-append (js-expr rator) "(" (comma* (js-expr* rand*)) ")"))
     (`(?: ,test ,true ,false)
-      (string-append
-        (js-expr ctx test) "?" (js-expr ctx true) ":" (js-expr ctx false)))
-    (`(get ,ref) (js-ref/lhs js-expr ctx ref))
-    (`(put ,lhs ,rhs) (js-put "=" ctx lhs rhs))
-    (`(put+ ,lhs ,rhs) (js-put "+=" ctx lhs rhs))
-    (`(put- ,lhs ,rhs) (js-put "-=" ctx lhs rhs))
-    (`(put* ,lhs ,rhs) (js-put "*=" ctx lhs rhs))
-    (`(put/ ,lhs ,rhs) (js-put "/=" ctx lhs rhs))
-    (`(put% ,lhs ,rhs) (js-put "%=" ctx lhs rhs))
-    (`(prefix++ ,lhs) (string-append "(++" (js-ref ctx lhs) ")"))
-    (`(prefix-- ,lhs) (string-append "(--" (js-ref ctx lhs) ")"))
-    (`(postfix++ ,lhs) (string-append "(" (js-ref ctx lhs) "++)"))
-    (`(postfix-- ,lhs) (string-append "(" (js-ref ctx lhs) "--)"))
-    (`(typeof ,expr) (string-append "(typeof " (js-expr ctx expr) ")"))
-    (`(instanceof ,instance ,class)
-      (js-binop ctx " instanceof " instance class))
-    (`(in ,ea ,eb) (js-binop ctx " in " ea eb))
-    (`(- ,expr) (js-unop ctx "-" expr))
-    (`(- ,ea ,eb) (js-binop ctx "-" ea eb))
-    (`(+ ,ea ,eb) (js-binop ctx "+" ea eb))
-    (`(* ,ea ,eb) (js-binop ctx "*" ea eb))
-    (`(/ ,ea ,eb) (js-binop ctx "/" ea eb))
-    (`(% ,ea ,eb) (js-binop ctx "%" ea eb))
-    (`(bneg ,expr) (js-unop ctx "~" expr))
-    (`(band ,ea ,eb) (js-binop ctx "&" ea eb))
-    (`(bor ,ea ,eb) (js-binop ctx "|" ea eb))
-    (`(bxor ,ea ,eb) (js-binop ctx "^" ea eb))
-    (`(bshl ,ea ,eb) (js-binop ctx "<<" ea eb))
-    (`(bshrs ,ea ,eb) (js-binop ctx ">>" ea eb))
-    (`(bshru ,ea ,eb) (js-binop ctx ">>>" ea eb))
-    (`(lneg ,expr) (js-unop ctx "!" expr))
-    (`(land ,ea ,eb) (js-binop ctx "&&" ea eb))
-    (`(lor ,ea ,eb) (js-binop ctx "||" ea eb))
-    (`(=== ,ea ,eb) (js-binop ctx "===" ea eb))
-    (`(== ,ea ,eb) (js-binop ctx "==" ea eb))
-    (`(!== ,ea ,eb) (js-binop ctx "!==" ea eb))
-    (`(!= ,ea ,eb) (js-binop ctx "!=" ea eb))
-    (`(> ,ea ,eb) (js-binop ctx ">" ea eb))
-    (`(>= ,ea ,eb) (js-binop ctx ">=" ea eb))
-    (`(< ,ea ,eb) (js-binop ctx "<" ea eb))
-    (`(<= ,ea ,eb) (js-binop ctx "<=" ea eb))
-    (`(comma ,ea ,eb) (js-binop ctx "," ea eb))
+      (string-append (js-expr test) "?" (js-expr true) ":" (js-expr false)))
+    (`(get ,ref) (js-ref/lhs js-expr ref))
+    (`(put ,lhs ,rhs) (js-put "=" lhs rhs))
+    (`(put+ ,lhs ,rhs) (js-put "+=" lhs rhs))
+    (`(put- ,lhs ,rhs) (js-put "-=" lhs rhs))
+    (`(put* ,lhs ,rhs) (js-put "*=" lhs rhs))
+    (`(put/ ,lhs ,rhs) (js-put "/=" lhs rhs))
+    (`(put% ,lhs ,rhs) (js-put "%=" lhs rhs))
+    (`(prefix++ ,lhs) (string-append "(++" (js-ref lhs) ")"))
+    (`(prefix-- ,lhs) (string-append "(--" (js-ref lhs) ")"))
+    (`(postfix++ ,lhs) (string-append "(" (js-ref lhs) "++)"))
+    (`(postfix-- ,lhs) (string-append "(" (js-ref lhs) "--)"))
+    (`(typeof ,expr) (string-append "(typeof " (js-expr expr) ")"))
+    (`(instanceof ,instance ,class) (js-binop " instanceof " instance class))
+    (`(in ,ea ,eb) (js-binop " in " ea eb))
+    (`(- ,expr) (js-unop "-" expr))
+    (`(- ,ea ,eb) (js-binop "-" ea eb))
+    (`(+ ,ea ,eb) (js-binop "+" ea eb))
+    (`(* ,ea ,eb) (js-binop "*" ea eb))
+    (`(/ ,ea ,eb) (js-binop "/" ea eb))
+    (`(% ,ea ,eb) (js-binop "%" ea eb))
+    (`(bneg ,expr) (js-unop "~" expr))
+    (`(band ,ea ,eb) (js-binop "&" ea eb))
+    (`(bor ,ea ,eb) (js-binop "|" ea eb))
+    (`(bxor ,ea ,eb) (js-binop "^" ea eb))
+    (`(bshl ,ea ,eb) (js-binop "<<" ea eb))
+    (`(bshrs ,ea ,eb) (js-binop ">>" ea eb))
+    (`(bshru ,ea ,eb) (js-binop ">>>" ea eb))
+    (`(lneg ,expr) (js-unop "!" expr))
+    (`(land ,ea ,eb) (js-binop "&&" ea eb))
+    (`(lor ,ea ,eb) (js-binop "||" ea eb))
+    (`(=== ,ea ,eb) (js-binop "===" ea eb))
+    (`(== ,ea ,eb) (js-binop "==" ea eb))
+    (`(!== ,ea ,eb) (js-binop "!==" ea eb))
+    (`(!= ,ea ,eb) (js-binop "!=" ea eb))
+    (`(> ,ea ,eb) (js-binop ">" ea eb))
+    (`(>= ,ea ,eb) (js-binop ">=" ea eb))
+    (`(< ,ea ,eb) (js-binop "<" ea eb))
+    (`(<= ,ea ,eb) (js-binop "<=" ea eb))
+    (`(comma ,ea ,eb) (js-binop "," ea eb))
     (`(delete ,expr) (js-unop "delete " expr))))
