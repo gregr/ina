81f0fda1b5e899b2ab9d1080b799d61a5866981e
gregr <greg.weiqi@gmail.com>
2017-12-21 16:15:59 -0500

    Define variable names with mostly-nameless s-terms

--- scheme/s-term.rkt
+++ scheme/s-term.rkt
@@ -2,13 +2,12 @@
 (provide
   s-term?
   s-literal s-literal? s-literal-datum
-  s-identifier s-identifier? s-identifier-name
+  s-variable s-variable? s-variable-name
   s-primitive-operation s-primitive-operation? s-primitive-operation-op
   s-if s-if? s-if-c s-if-t s-if-f
   s-app s-app? s-app-proc s-app-arg*
-  s-lambda s-lambda? s-lambda-param* s-lambda-body
+  s-lambda s-lambda? s-lambda-body
   s-letfix s-letfix? s-letfix-binding* s-letfix-body
-
 
   s-syntax s-syntax? s-syntax-body
   s-quasisyntax s-quasisyntax? s-quasisyntax-body
@@ -16,19 +15,23 @@
   s-unsyntax-splicing s-unsyntax-splicing? s-unsyntax-splicing-body
 
   s-set! s-set!? s-set!-name s-set!-body
-  s-shift s-shift? s-shift-tag s-shift-name s-shift-body
+  s-shift s-shift? s-shift-tag s-shift-body
   s-reset s-reset? s-reset-tag s-reset-body
+
+  name?
+  name-bound name-bound? name-bound-address
+  name-free name-free? name-free-address
   )
 (require "record.rkt")
 
 (define-record-variant
   s-term?
   (s-literal s-literal? s-literal-datum)
-  (s-identifier s-identifier? s-identifier-name)
+  (s-variable s-variable? s-variable-name)
   (s-primitive-operation s-primitive-operation? s-primitive-operation-op)
   (s-if s-if? s-if-c s-if-t s-if-f)
   (s-app s-app? s-app-proc s-app-arg*)
-  (s-lambda s-lambda? s-lambda-param* s-lambda-body)
+  (s-lambda s-lambda? s-lambda-body)
   (s-letfix s-letfix? s-letfix-binding* s-letfix-body)
 
   (s-syntax s-syntax? s-syntax-body)
@@ -37,6 +40,12 @@
   (s-unsyntax-splicing s-unsyntax-splicing? s-unsyntax-splicing-body)
 
   (s-set! s-set!? s-set!-name s-set!-body)
-  (s-shift s-shift? s-shift-tag s-shift-name s-shift-body)
+  (s-shift s-shift? s-shift-tag s-shift-body)
   (s-reset s-reset? s-reset-tag s-reset-body)
+  )
+
+(define-record-variant
+  name?
+  (name-bound name-bound? name-bound-address)
+  (name-free name-free? name-free-address)
   )
