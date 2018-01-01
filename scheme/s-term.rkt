377c175d6a82fc389a341165462e1cd54d415ae2
gregr <greg.weiqi@gmail.com>
2017-12-31 23:13:27 -0500

    Replace names with ids

--- scheme/s-term.rkt
+++ scheme/s-term.rkt
@@ -2,11 +2,11 @@
 (provide
   s-term?
   s-literal s-literal? s-literal-datum
-  s-variable s-variable? s-variable-name
+  s-variable s-variable? s-variable-id
   s-primitive-operation s-primitive-operation? s-primitive-operation-op
   s-if s-if? s-if-c s-if-t s-if-f
   s-app s-app? s-app-proc s-app-arg*
-  s-lambda s-lambda? s-lambda-body
+  s-lambda s-lambda? s-lambda-id s-lambda-body
   s-letfix s-letfix? s-letfix-binding* s-letfix-body
 
   s-syntax s-syntax? s-syntax-body
@@ -16,21 +16,17 @@
 
   s-shift s-shift? s-shift-body
   s-reset s-reset? s-reset-body
-
-  name?
-  name-bound name-bound? name-bound-address
-  name-free name-free? name-free-address
   )
 (require "record.rkt")
 
 (define-record-variant
   s-term?
   (s-literal s-literal? s-literal-datum)
-  (s-variable s-variable? s-variable-name)
+  (s-variable s-variable? s-variable-id)
   (s-primitive-operation s-primitive-operation? s-primitive-operation-op)
   (s-if s-if? s-if-c s-if-t s-if-f)
   (s-app s-app? s-app-proc s-app-arg*)
-  (s-lambda s-lambda? s-lambda-body)
+  (s-lambda s-lambda? s-lambda-id s-lambda-body)
   (s-letfix s-letfix? s-letfix-binding* s-letfix-body)
 
   (s-syntax s-syntax? s-syntax-body)
@@ -40,10 +36,4 @@
 
   (s-shift s-shift? s-shift-body)
   (s-reset s-reset? s-reset-body)
-  )
-
-(define-record-variant
-  name?
-  (name-bound name-bound? name-bound-address)
-  (name-free name-free? name-free-address)
   )
