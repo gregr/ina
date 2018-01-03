473a28641f19ae6a3b864f2c4a009a13f6d67a55
gregr <greg.weiqi@gmail.com>
2018-01-03 14:22:05 -0500

    Reimplement records as structs
    This way we can disambiguate when comparing with vectors.

--- scheme/record.rkt
+++ scheme/record.rkt
@@ -2,33 +2,44 @@
 (provide
   define-record
   define-record-variant
+  record?
+  record-payload
+  record-tag
   )
 (require
+  racket/struct
   racket/vector
   )
+
+(struct record (tag payload)
+        #:methods gen:custom-write
+        ((define write-proc
+           (make-constructor-style-printer
+             (lambda (d) (record-tag d))
+             (lambda (d) (vector->list (record-payload d)))))))
 
 (define-syntax define-record
   (syntax-rules ()
     ((_ name name?)
-     (begin (define name (vector 'name))
+     (begin (define name (record 'name '#()))
             (define (name? datum) (eq? name datum))))
     ((_ name name? f* ...)
      (begin (define (name? datum)
-              (and (vector? datum) (eq? 'name (vector-ref datum 0))))
-            (define-record-etc name 1 (f* ...) ())))))
+              (and (record? datum) (eq? 'name (record-tag datum))))
+            (define-record-etc name 0 (f* ...) ())))))
 (define-syntax define-record-etc
   (syntax-rules ()
     ((_ name _ () (field* ...))
-     (define (name field* ...) (vector 'name field* ...)))
+     (define (name field* ...) (record 'name (vector field* ...))))
     ((_ name index ((get set) f* ...) field*)
      (begin (define-record-etc name index (get f* ...) field*)
             (define (set datum value)
-              (let ((new (vector-copy datum)))
+              (let ((new (vector-copy (record-payload datum))))
                 (vector-set! new index value)
-                new))))
+                (record 'name new)))))
      ((_ name index (get f* ...) (field* ...))
       (begin (define-record-etc name (+ 1 index) (f* ...) (field* ... get))
-             (define (get datum) (vector-ref datum index))))))
+             (define (get datum) (vector-ref (record-payload datum) index))))))
 
 (define-syntax define-record-variant
   (syntax-rules ()
