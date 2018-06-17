7cb4a059ecbe2e29c71789c1bde7bfd1aaaf05ca
gregr <greg.weiqi@gmail.com>
2018-06-16 21:56:47 -0400

    Factor out closure->procedure

--- scheme/bootstrap/eval-ast.rkt
+++ scheme/bootstrap/eval-ast.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 (provide
   eval-ast
+  closure->procedure
   )
 
 (require
@@ -34,19 +35,30 @@
            (cons name full-op)) primitive-ops)))
 
 ;;; Evaluation
-(define (evaluate-apply proc arg*)
-  (when (not (closure? proc)) (error "cannot apply non-procedure:" proc))
-  (define addr* (closure-param* proc))
+(define (closure->procedure clo)
+  (define addr* (closure-param* clo))
+  (define cenv (closure-env clo))
+  (define body (closure-body clo))
   (define (continue a*)
     (define b?* (map (lambda (addr a) (and addr (cons addr a))) addr* a*))
-    (define env (env-extend* (closure-env proc) (filter-not not b?*)))
-    (eval-ast/env env (closure-body proc)))
-  (define plen (length addr*)) (define alen (length arg*))
-  (cond ((and (closure-variadic? proc) (<= (- plen 1) alen))
-         (let ((a0* (take arg* (- plen 1))) (a1* (drop arg* (- plen 1))))
-           (continue (append a0* (list a1*)))))
-        ((= plen alen) (continue arg*))
-        (else (error "argument count mismatch:" plen alen proc arg*))))
+    (define env (env-extend* cenv (filter-not not b?*)))
+    (eval-ast/env env body))
+  (define plen (length addr*))
+  (cond ((closure-variadic? clo)
+         (lambda (arg*)
+           (when (< (length arg*) (- plen 1))
+             (error "too few arguments:" (- plen 1) (length arg*) clo arg*))
+           (let ((a0* (take arg* (- plen 1))) (a1* (drop arg* (- plen 1))))
+             (continue (append a0* (list a1*))))))
+        (else (lambda (arg*)
+                (when (not (= (length arg*) plen))
+                  (error "arity mismatch:" plen (length arg*) clo arg*))
+                (continue arg*)))))
+
+(define (evaluate-apply proc arg*)
+  ((cond ((procedure? proc) proc)
+         ((closure? proc) (closure->procedure proc))
+         (else (error "cannot apply non-procedure:" proc))) arg*))
 
 (define (eval-ast/env env tm)
   (define (loop tm) (eval-ast/env env tm))
