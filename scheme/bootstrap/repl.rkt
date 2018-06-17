d24e6030524fc2ddaadd6549dd1738519168d301
gregr <greg.weiqi@gmail.com>
2018-06-16 21:16:40 -0400

    Create bootstrap/repl.rkt

---
+++ scheme/bootstrap/repl.rkt
@@ -0,0 +1,15 @@
+#lang racket/base
+(provide repl)
+
+(require
+  "eval-ast.rkt"
+  "expand.rkt"
+  readline
+  readline/pread
+  )
+
+(define (repl)
+  (define form (parameterize ((readline-prompt #"REPL> ")) (read)))
+  (if (eof-object? form) (printf "exiting REPL\n")
+    (begin (printf "~s\n" (eval-ast (expand form)))
+           (repl))))
