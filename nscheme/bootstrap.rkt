5864888a638fec77aea3313be575768b80e961af
gregr <greg.weiqi@gmail.com>
2018-09-10 18:23:51 -0400

    Include nscheme module in nscheme library

--- nscheme/bootstrap.rkt
+++ nscheme/bootstrap.rkt
@@ -7,7 +7,7 @@
 
 (define db:lib
   (for/list ((src '((data box tagged assoc compare)
-                    (nscheme data ast ast-eval))))
+                    (nscheme data ast ast-eval nscheme))))
     (define library-name (car src))
     `(,library-name
        . ,(for/list ((module-name (cdr src)))
