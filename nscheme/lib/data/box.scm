a5f669118ce53ec979ba37c23c47728df7409c75
gregr <greg.weiqi@gmail.com>
2018-09-11 20:31:18 -0400

    Define box?

--- nscheme/lib/data/box.scm
+++ nscheme/lib/data/box.scm
@@ -1,8 +1,10 @@
 (provide
   box
+  box?
   unbox
   set-box!)
 
 (define (box d)         (make-mvector 1 d))
+(define (box? d)        (and (mvector? d) (= 1 (mvector-length d))))
 (define (unbox mv)      (mvector-ref mv 0))
 (define (set-box! mv d) (mvector-set! mv 0 d))
