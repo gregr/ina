d379612ff7a079ecf9c50f5350a450d101cbffb6
gregr <greg.weiqi@gmail.com>
2018-09-25 18:37:22 -0400

    Move env:future construction

--- nscheme/lib/nscheme/nscheme-future.scm
+++ nscheme/lib/nscheme/nscheme-future.scm
@@ -1,8 +1,181 @@
 (provide test!)
 
-(require eval/ast nscheme:expand)
+(require eval/ast expand expander nscheme:expand env:base)
 
-(define (ev form) (eval/ast (nscheme:expand form)))
+;; TODO: define these in userspace after bootstrapping $-based extension.
+,(
+(define (parser-descs->b* qenv descs)
+  (cons 'list
+        (map (lambda (name&clause*)
+               (list 'cons (list 'quote (car name&clause*))
+                     (list 'lambda '(env form)
+                           '(define (ex form) (expand env form))
+                           '(define (@ i) (list-ref form i))
+                           '(define (@. i) (list-tail form i))
+                           (cons 'cond (append
+                                         (cdr name&clause*)
+                                         '((else (error '"invalid syntax:"
+                                                        form))))))))
+             descs)))
+
+;; Parsers with dependencies on base definitions
+(define parsers:future
+  '((case ((length>=? 3 form)
+           (define =? (make-syntax=? ctx:var env:future env))
+           (expand:let/temp
+             'scrutinee env (@ 1)
+             (lambda (x)
+               (unless (length>=? 1 (@. 2)) (error '"invalid case:" form))
+               (let loop ((c* (@. 2)))
+                 (cond
+                   ((null? c*) ($error (ast:quote '"no matching case clause:")
+                                       x (ast:quote form)))
+                   ((not (length>=? 2 (car c*))) (error '"invalid case:" form))
+                   ((and (=? 'else (caar c*)) (length=? 1 c*))
+                    (expand:body* env (cdar c*)))
+                   ((list? (caar c*))
+                    (ast:if
+                      (foldr (lambda (d r) (ast:if ($equal? x d) $true r))
+                             $false (map ast:quote (caar c*)))
+                      (expand:body* env (cdar c*)) (loop (cdr c*))))
+                   (else (error '"invalid case:" form))))))))
+    (quasiquote
+      ((length=? 2 form)
+       (define (tag t e) ($cons (ast:quote t) ($cons e (ast:quote '()))))
+       (define =? (make-syntax=? ctx:var env:future env))
+       (define (t? tag d) (and (length=? 2 d) (=? tag (car d))))
+       (let loop ((level 0) (qqf (@ 1)))
+         (cond ((t? 'quasiquote qqf)
+                (tag 'quasiquote (loop (+ level 1) (cadr qqf))))
+               ((t? 'unquote qqf)
+                (if (= 0 level) (ex (cadr qqf))
+                  (tag 'unquote (loop (- level 1) (cadr qqf)))))
+               ((and (pair? qqf) (t? 'unquote-splicing (car qqf)))
+                (define qqd (loop level (cdr qqf)))
+                (if (= 0 level) ($append (ex (cadar qqf)) qqd)
+                  ($cons (tag 'unquote-splicing
+                              (loop (- level 1) (cadar qqf))) qqd)))
+               ((vector? qqf) ($list->vector (loop level (vector->list qqf))))
+               ((pair? qqf)   ($cons (loop level (car qqf))
+                                     (loop level (cdr qqf))))
+               ((ormap (lambda (t) (=? t qqf))
+                       '(quasiquote unquote unquote-splicing))
+                (error '"malformed quasiquote:" qqf form))
+               (else (ast:quote qqf))))))
+    (match/=? ((length>=? 4 form)
+               (parse:match/=? env (@ 1) (@ 2) (@. 3) form)))
+    (match ((length>=? 3 form)
+            (parse:match/=? env (expander (lambda _ $var:equal?))
+                            (@ 1) (@. 2) form)))))
+
+(append
+  '(begin
+     (define (parse:match/=? env =?-e scrutinee body* full-form)
+       (define (parse:clause $=? $x env pat rhs $k-fail)
+         (define $fail ($apply* $k-fail '()))
+         (define ($try $c $k) (ast:if $c $k $fail))
+         (define ($succeed env)
+           (define =? (make-syntax=? ctx:var env:future env))
+           (cond ((and (pair? rhs) (length>=? 1 (car rhs))
+                       (=? 'guard (caar rhs)))
+                  ($try (expand:and* env (cdar rhs))
+                        (expand:body* env (cdr rhs))))
+                 (else (expand:body* env rhs))))
+         (define (tqq datum) (list 'quasiquote datum))
+         (let loop (($x $x) (p pat) (env env) ($succeed $succeed))
+           (define ($try-vec pat)
+             (define (k $x) (loop $x pat env $succeed))
+             ($try ($vector? $x) ($let/temp 'x ($vector->list $x) k)))
+           (let retry ((p p))
+             (define (? tag) (equal? (car p) tag))
+             (define (?= n tag) (and (? tag) (length=? n p)))
+             (define (?>= n tag) (and (? tag) (length>=? n p)))
+             (cond
+               ((equal? p '_)                 ($succeed env))
+               ((name? p)                     (retry (list 'var p)))
+               ((or (boolean? p) (number? p)) (retry (list 'quote p)))
+               ((not (length>=? 1 p)) (error '"invalid pattern:" pat p))
+               ((and (?= 2 'var) (name? (cadr p)))
+                ($let (list (cadr p)) (list $x)
+                      (lambda (b*) ($succeed (env-extend*/var env b*)))))
+               ((?= 2 'quote)
+                ($try ($apply* $=? (list $x (ast:quote (cadr p))))
+                      ($succeed env)))
+               ((?= 3 'cons)
+                (define ($k env)
+                  ($let/temp 'x-cdr ($cdr $x)
+                             (lambda ($x) (loop $x (caddr p) env $succeed))))
+                ($try ($pair? $x) ($let/temp 'x-car ($car $x)
+                                             (lambda ($x)
+                                               (loop $x (cadr p) env $k)))))
+               ((?=  2 'list*) (retry (cadr p)))
+               ((?>= 3 'list*) (retry (list 'cons (cadr p)
+                                            (cons 'list* (cddr p)))))
+               ((? 'list)   (retry (cons 'list* (append (cdr p) (list ''())))))
+               ((? 'vector) ($try-vec (cons 'list (cdr p))))
+               ((?= 2 'quasiquote)
+                (define qq (cadr p))
+                (cond ((vector? qq) ($try-vec (tqq (vector->list qq))))
+                      ((and (length=? 1 qq) (length=? 2 (car qq))
+                            (equal? (caar qq) 'unquote-splicing)
+                            (name? (cadar qq)))
+                       ($try ($list? $x) (retry (cadar qq))))
+                      ((and (length=? 2 qq) (equal? (car qq) 'unquote))
+                       (retry (cadr qq)))
+                      ((pair? qq)
+                       (retry (list 'cons (tqq (car qq)) (tqq (cdr qq)))))
+                      ((ormap (lambda (d) (equal? qq d))
+                              '(quasiquote unquote unquote-splicing))
+                       (error '"bad quasiquote keyword:" qq pat))
+                      (else (retry (list 'quote qq)))))
+               (else (error '"invalid pattern:" pat p))))))
+       (define (parse:clauses $=? env c*)
+         ($lambda/temp
+           'scrutinee
+           (lambda ($x)
+             (foldr
+               (lambda (c $fail)
+                 (cond ((length>=? 2 c)
+                        ($let/temp
+                          'k-fail ($thunk $fail)
+                          (lambda ($kf) (parse:clause
+                                          $=? $x env (car c) (cdr c) $kf))))
+                       (error (error '"invalid match clause:" c))))
+               ($error (ast:quote '"no matching clause:")
+                       $x (ast:quote full-form)) c*))))
+       (expand:let/temp
+         '=? env =?-e
+         (lambda ($=?)
+           ($apply*
+             (cond ((name? (car body*))
+                    (define name (car body*))
+                    (define (e env) (parse:clauses $=? env (cdr body*)))
+                    (expand:letrec env (list name) (list (expander e))
+                                   (lambda (env) (parse:var env name))))
+                   (else (parse:clauses $=? env body*)))
+             (list (expand env scrutinee)))))))
+
+  (list (list 'define 'env:future
+              (list 'env-extend*/syntax
+                    'env:base 'ctx:op
+                    (parser-descs->b* 'env:future parsers:future))))
+
+ '((define ($equal? a b)     ($apply* $var:equal? (list a b)))
+   (define ($list? x)        ($apply* $var:list?  (list x)))
+   (define ($append a b)     ($apply* $var:append (list a b)))
+   (define ($list->vector x) ($apply* $var:list->vector (list x)))
+   (define ($vector->list x) ($apply* $var:vector->list (list x)))
+
+   (define $var:equal?       (parse:var env:future 'equal?))
+   (define $var:list?        (parse:var env:future 'list?))
+   (define $var:append       (parse:var env:future 'append))
+   (define $var:list->vector (parse:var env:future 'list->vector))
+   (define $var:vector->list (parse:var env:future 'vector->list))))
+)
+
+(define (ev form) (eval/ast (nscheme:expand
+                              (expander (lambda (_)
+                                          (expand env:future form))))))
 
 (define (test! test)
   (test 'qq-1
--- nscheme/lib/nscheme/nscheme.scm
+++ nscheme/lib/nscheme/nscheme.scm
@@ -1,4 +1,4 @@
-(provide nscheme:expand)
+(provide nscheme:expand env:base)
 
 (require box tagged-vector? tagged-vector?!
          primitive-ops
@@ -294,20 +294,6 @@
         (reverse (foldl ucons '() xs))))))
 
 ,(
-(define (parser-descs->b* qenv descs)
-  (cons 'list
-        (map (lambda (name&clause*)
-               (list 'cons (list 'quote (car name&clause*))
-                     (list 'lambda '(env form)
-                           '(define (ex form) (expand env form))
-                           '(define (@ i) (list-ref form i))
-                           '(define (@. i) (list-tail form i))
-                           (cons 'cond (append
-                                         (cdr name&clause*)
-                                         '((else (error '"invalid syntax:"
-                                                        form))))))))
-             descs)))
-
 ;; Parsers for primitive syntax (no var dependencies)
 (define parsers:primitive
   '((apply ((length=? 3 form) (ast:apply (ex (@ 1)) (ex (@ 2)))))
@@ -370,155 +356,29 @@
     (unless ((length>=? 3 form)
              (ast:if (ex (@ 1)) $true (expand:body* env (@. 2)))))))
 
-;; TODO:
-;; let-alias, let-without|unlet, let-only|unlet-except[/var][/syntax][/all]
-;; import, export, and/let*
-
-(define parsers:base
-  '((case ((length>=? 3 form)
-           (define =? (make-syntax=? ctx:var env:base env))
-           (expand:let/temp
-             'scrutinee env (@ 1)
-             (lambda (x)
-               (unless (length>=? 1 (@. 2)) (error '"invalid case:" form))
-               (let loop ((c* (@. 2)))
-                 (cond
-                   ((null? c*) ($error (ast:quote '"no matching case clause:")
-                                       x (ast:quote form)))
-                   ((not (length>=? 2 (car c*))) (error '"invalid case:" form))
-                   ((and (=? 'else (caar c*)) (length=? 1 c*))
-                    (expand:body* env (cdar c*)))
-                   ((list? (caar c*))
-                    (ast:if
-                      (foldr (lambda (d r) (ast:if ($equal? x d) $true r))
-                             $false (map ast:quote (caar c*)))
-                      (expand:body* env (cdar c*)) (loop (cdr c*))))
-                   (else (error '"invalid case:" form))))))))
-    (quasiquote
-      ((length=? 2 form)
-       (define (tag t e) ($cons (ast:quote t) ($cons e (ast:quote '()))))
-       (define =? (make-syntax=? ctx:var env:base env))
-       (define (t? tag d) (and (length=? 2 d) (=? tag (car d))))
-       (let loop ((level 0) (qqf (@ 1)))
-         (cond ((t? 'quasiquote qqf)
-                (tag 'quasiquote (loop (+ level 1) (cadr qqf))))
-               ((t? 'unquote qqf)
-                (if (= 0 level) (ex (cadr qqf))
-                  (tag 'unquote (loop (- level 1) (cadr qqf)))))
-               ((and (pair? qqf) (t? 'unquote-splicing (car qqf)))
-                (define qqd (loop level (cdr qqf)))
-                (if (= 0 level) ($append (ex (cadar qqf)) qqd)
-                  ($cons (tag 'unquote-splicing
-                              (loop (- level 1) (cadar qqf))) qqd)))
-               ((vector? qqf) ($list->vector (loop level (vector->list qqf))))
-               ((pair? qqf)   ($cons (loop level (car qqf))
-                                     (loop level (cdr qqf))))
-               ((ormap (lambda (t) (=? t qqf))
-                       '(quasiquote unquote unquote-splicing))
-                (error '"malformed quasiquote:" qqf form))
-               (else (ast:quote qqf))))))
-    (match/=? ((length>=? 4 form)
-               (parse:match/=? env (@ 1) (@ 2) (@. 3) form)))
-    (match ((length>=? 3 form)
-            (parse:match/=? env (expander (lambda _ $var:equal?))
-                            (@ 1) (@. 2) form)))))
+(define (parser-descs->b* qenv descs)
+  (cons 'list
+        (map (lambda (name&clause*)
+               (list 'cons (list 'quote (car name&clause*))
+                     (list 'lambda '(env form)
+                           '(define (ex form) (expand env form))
+                           '(define (@ i) (list-ref form i))
+                           '(define (@. i) (list-tail form i))
+                           (cons 'cond (append
+                                         (cdr name&clause*)
+                                         '((else (error '"invalid syntax:"
+                                                        form))))))))
+             descs)))
 
 ;; Environment construction
 (append
-  '(begin
-     (define (parse:match/=? env =?-e scrutinee body* full-form)
-       (define (parse:clause $=? $x env pat rhs $k-fail)
-         (define $fail ($apply* $k-fail '()))
-         (define ($try $c $k) (ast:if $c $k $fail))
-         (define ($succeed env)
-           (define =? (make-syntax=? ctx:var env:base env))
-           (cond ((and (pair? rhs) (length>=? 1 (car rhs))
-                       (=? 'guard (caar rhs)))
-                  ($try (expand:and* env (cdar rhs))
-                        (expand:body* env (cdr rhs))))
-                 (else (expand:body* env rhs))))
-         (define (tqq datum) (list 'quasiquote datum))
-         (let loop (($x $x) (p pat) (env env) ($succeed $succeed))
-           (define ($try-vec pat)
-             (define (k $x) (loop $x pat env $succeed))
-             ($try ($vector? $x) ($let/temp 'x ($vector->list $x) k)))
-           (let retry ((p p))
-             (define (? tag) (equal? (car p) tag))
-             (define (?= n tag) (and (? tag) (length=? n p)))
-             (define (?>= n tag) (and (? tag) (length>=? n p)))
-             (cond
-               ((equal? p '_)                 ($succeed env))
-               ((name? p)                     (retry (list 'var p)))
-               ((or (boolean? p) (number? p)) (retry (list 'quote p)))
-               ((not (length>=? 1 p)) (error '"invalid pattern:" pat p))
-               ((and (?= 2 'var) (name? (cadr p)))
-                ($let (list (cadr p)) (list $x)
-                      (lambda (b*) ($succeed (env-extend*/var env b*)))))
-               ((?= 2 'quote)
-                ($try ($apply* $=? (list $x (ast:quote (cadr p))))
-                      ($succeed env)))
-               ((?= 3 'cons)
-                (define ($k env)
-                  ($let/temp 'x-cdr ($cdr $x)
-                             (lambda ($x) (loop $x (caddr p) env $succeed))))
-                ($try ($pair? $x) ($let/temp 'x-car ($car $x)
-                                             (lambda ($x)
-                                               (loop $x (cadr p) env $k)))))
-               ((?=  2 'list*) (retry (cadr p)))
-               ((?>= 3 'list*) (retry (list 'cons (cadr p)
-                                            (cons 'list* (cddr p)))))
-               ((? 'list)   (retry (cons 'list* (append (cdr p) (list ''())))))
-               ((? 'vector) ($try-vec (cons 'list (cdr p))))
-               ((?= 2 'quasiquote)
-                (define qq (cadr p))
-                (cond ((vector? qq) ($try-vec (tqq (vector->list qq))))
-                      ((and (length=? 1 qq) (length=? 2 (car qq))
-                            (equal? (caar qq) 'unquote-splicing)
-                            (name? (cadar qq)))
-                       ($try ($list? $x) (retry (cadar qq))))
-                      ((and (length=? 2 qq) (equal? (car qq) 'unquote))
-                       (retry (cadr qq)))
-                      ((pair? qq)
-                       (retry (list 'cons (tqq (car qq)) (tqq (cdr qq)))))
-                      ((ormap (lambda (d) (equal? qq d))
-                              '(quasiquote unquote unquote-splicing))
-                       (error '"bad quasiquote keyword:" qq pat))
-                      (else (retry (list 'quote qq)))))
-               (else (error '"invalid pattern:" pat p))))))
-       (define (parse:clauses $=? env c*)
-         ($lambda/temp
-           'scrutinee
-           (lambda ($x)
-             (foldr
-               (lambda (c $fail)
-                 (cond ((length>=? 2 c)
-                        ($let/temp
-                          'k-fail ($thunk $fail)
-                          (lambda ($kf) (parse:clause
-                                          $=? $x env (car c) (cdr c) $kf))))
-                       (error (error '"invalid match clause:" c))))
-               ($error (ast:quote '"no matching clause:")
-                       $x (ast:quote full-form)) c*))))
-       (expand:let/temp
-         '=? env =?-e
-         (lambda ($=?)
-           ($apply*
-             (cond ((name? (car body*))
-                    (define name (car body*))
-                    (define (e env) (parse:clauses $=? env (cdr body*)))
-                    (expand:letrec env (list name) (list (expander e))
-                                   (lambda (env) (parse:var env name))))
-                   (else (parse:clauses $=? env body*)))
-             (list (expand env scrutinee)))))))
-
   (list
+    'begin
     (list
       'define 'env:primitive-syntax
-      (list
-        'env-extend*/syntax
+      (list 'env-extend*/syntax
         (list 'env-extend*/syntax
-              (list 'env-extend*/syntax
-                    'env:empty 'ctx:op
+              (list 'env-extend*/syntax 'env:empty 'ctx:op
                     (parser-descs->b*
                       'env:primitive-syntax parsers:primitive))
               'ctx:def '(list (cons 'begin parse:begin)
@@ -534,7 +394,6 @@
                               (else (error '"invalid primitive op:"
                                            po-desc form))))))
               primitive-ops))))
-
 
  '((define env:base #t)
    (define nscheme:expand
@@ -552,23 +411,5 @@
                                         (lambda (env)
                                           (set! env:base (env-freeze env))
                                           (define form (shift k k))
-                                          (expand env:base form))))))))))
-
-   ;; Parsers with dependencies on base definitions
-   (define ($equal? a b)     ($apply* $var:equal? (list a b)))
-   (define ($list? x)        ($apply* $var:list?  (list x)))
-   (define ($append a b)     ($apply* $var:append (list a b)))
-   (define ($list->vector x) ($apply* $var:list->vector (list x)))
-   (define ($vector->list x) ($apply* $var:vector->list (list x)))
-
-   (define $var:equal?       (parse:var env:base 'equal?))
-   (define $var:list?        (parse:var env:base 'list?))
-   (define $var:append       (parse:var env:base 'append))
-   (define $var:list->vector (parse:var env:base 'list->vector))
-   (define $var:vector->list (parse:var env:base 'vector->list)))
-
-  (list (list 'set! 'env:base
-              (list 'env-extend*/syntax
-                    'env:base 'ctx:op
-                    (parser-descs->b* 'env:base parsers:base)))))
+                                          (expand env:base form))))))))))))
 )
