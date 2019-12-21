8df20cbba1cd09433391cd6aba8e93982e00fcbc
gregr <greg.weiqi@gmail.com>
2019-12-21 10:05:48 -0500

    Define parse/dfs

--- nscheme/grammar.scm
+++ nscheme/grammar.scm
@@ -26,22 +26,23 @@
                     (begin (define-tuple dd) (define-tuple dds) ...))
 
 (define (grammar? x) (vector? x))
-(define-tuple* (g:fail        msg       )
-               (g:eps         v         )
-               (g:one         ?         )
-               (g:alt         g1   g2   )
-               (g:seq         g1   g2   )
-               (g:app         proc g    )
-               (g:bind        k    g    )
-               (g:peek        g         )
-               (g:peek-not    g         )
-               (g:nonterminal meta thunk))
+(define-tuple* (g:fail        reasons     )
+               (g:eps         v-thunk     )
+               (g:one         meta ?      )
+               (g:alt         g1   g2     )
+               (g:seq         g1   g2     )
+               (g:app         proc g      )
+               (g:bind        v->g g      )
+               (g:peek        g           )
+               (g:peek-not    g           )
+               (g:nonterminal meta g-thunk))
 
 (define-syntax-rule (return x) (g:eps (thunk x)))
-(define (fail . xs)           (g:fail xs))
-(define any                   (g:one (lambda (_) #t)))
-(define (one v)               (g:one (lambda (x) (equal? v x))))
-(define (in-range low high)   (g:one (lambda (x) (and x (<= low x high)))))
+(define (fail . xs)            (g:fail (list xs)))
+(define any                    (g:one (list 'any)   (lambda (_) #t)))
+(define (one v)                (g:one (list 'one v) (lambda (x) (equal? v x))))
+(define (in-range low high)    (g:one (list 'in-range low high)
+                                      (lambda (x) (and x (<= low x high)))))
 
 (define (g:lift x)
   (cond ((grammar? x) x)
@@ -102,9 +103,72 @@
                                           (xs (apply */seq gs)))
                                (cons x xs)))
 
-;; TODO: first-order representation to support various parsing strategies
-;; start with a DFS strategy that will work with non-left-recursive grammars
-;; for more expressiveness, try
+(define (grammar->parser/dfs g)
+  (match g
+    (`#(g:fail ,reasons) (lambda (k kf . _)     (kf reasons)))
+    (`#(g:eps  ,thunk  ) (lambda (k kf s peek?) (k s thunk)))
+    (`#(g:one  ,meta ,?) (lambda (k kf s peek?)
+                           (let* ((s (s peek?)) (v (car s)))
+                             (if (? v) (k (cdr s) (thunk v))
+                               (kf (list (cons v meta)))))))
+    (`#(g:alt ,g1 ,g2)
+      (define p1 (grammar->parser/dfs g1))
+      (define p2 (grammar->parser/dfs g2))
+      (lambda (k kf s peek?)
+        (p1 k (lambda (r1) (p2 k (lambda (r2) (kf (append r1 r2))) s peek?))
+            s peek?)))
+    (`#(g:seq ,g1 ,g2)
+      (define p1 (grammar->parser/dfs g1))
+      (define p2 (grammar->parser/dfs g2))
+      (lambda (k kf s peek?)
+        (p1 (lambda (s r1) (p2 (lambda (s r2) (k s (thunk (cons (r1) (r2)))))
+                               kf s peek?))
+            kf s peek?)))
+    (`#(g:app ,proc ,g)
+      (define p (grammar->parser/dfs g))
+      (lambda (k kf s peek?) (p (lambda (s r) (k s (thunk (proc (r)))))
+                                kf s peek?)))
+    (`#(g:bind ,v->g ,g)
+      (define p (grammar->parser/dfs g))
+      (lambda (k kf s peek?)
+        (p (lambda (s r) ((grammar->parser/dfs (v->g (r))) k s peek?))
+           kf s peek?)))
+    (`#(g:peek     ,g) (define p (grammar->parser/dfs g))
+                       (lambda (k kf s _) (p (lambda (_ r) (k s r)) kf s #t)))
+    (`#(g:peek-not ,g) (define p (grammar->parser/dfs g))
+                       (lambda (k kf s _)
+                         (p (lambda (_ r) (kf (list r)))
+                            (lambda (r)   (k s (thunk #t))) s #t)))
+    (`#(g:nonterminal ,meta ,gthunk)
+      (lambda (k kf s peek?) ((grammar->parser/dfs (gthunk)) k kf s peek?)))))
+
+(define (parse/dfs g in k kf)
+  ;; TODO: this commit strategy is too eager.  For a set of alternatives, we
+  ;; should only commit the amount consumed by the chosen match.  We can fix
+  ;; this in a few ways:
+  ;;   * thread a progress counter telling us how much to commit at the end
+  ;;     * a downside is the need to retain uncommitted buffers for longer
+  ;;   * use BFS instead of DFS, committing progress of the slowest branch
+  ;;     * more complicated, but buffers may be committed incrementally
+  ;;       * peek node retains a buffer to provide to downstream consumer
+  ;;       * max retained buffer size determines global uncommitted amount
+  ;;     * this is closer to the incremental parsing we eventually want
+  (define lookahead 0)
+  (define (stream)
+    (let ((v #f) (committed? #f))
+      (lambda (peek?)
+        (cond ((and v (or committed? peek?)) v)
+              (v (set! committed? #t) (set! lookahead (- lookahead 1))
+                 (in 'get) v)
+              (peek? (set! v (cons (in 'peek lookahead) (stream)))
+                     (set! lookahead (+ lookahead 1))
+                     v)
+              (else  (set! committed? #t)
+                     (set! v (cons (in 'get)            (stream)))
+                     v)))))
+  ((grammar->parser/dfs g) (lambda (_ r) (k r)) kf (stream) #f))
+
+;; TODO: for more expressiveness, try
 ;;   BFS to handle left-recursion as simply as possible
 ;;   CPS memoization (i.e., tabling support for left-recursion)
 ;;   parsing with derivatives
@@ -115,3 +179,5 @@
 ;; ensure that pusher can distinguish states which peek vs consume
 ;;   components of intermediate states must indicate size of unconsumed buffer
 ;;   pushers only commit values that are consumed by at least one component
+
+;; TODO: grammar compaction via smart constructors?
