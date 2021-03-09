40bec1c356b6cc59ebdeba1ebec3dc35db6634fa
gregr <greg.weiqi@gmail.com>
2021-03-09 16:24:49 -0500

    Improve larger-log10 some more

--- nscheme/write.scm
+++ nscheme/write.scm
@@ -52,6 +52,9 @@
         ((procedure? v) (pr "#<procedure>"))))
 
 ;; TODO: reduce dependency on exact->inexact and inexact->exact
+;; manipulate floating point representation directly via:
+;;   (real->floating-point-bytes x 8)
+;;   (floating-point-bytes->real bs)
 (define (write-number n out)
   (define (pr s)       (out 'put* s))
   (define (pr-digit d) (out 'put (+ d (char "0"))))
@@ -63,18 +66,12 @@
   (define (pr-int n) (cond ((< n 0) (pr "-") (pr-nat (- n)))
                            (else             (pr-nat    n))))
   (define (larger-log10 n)
-    (if (< n 1) 0
-      (let loop ((e 1) (p 10) (e+ 1) (p+ 10) (prev '()))
-        (if (< n p+)
-          (let loop ((e e) (p p) (e.best e+) (p.best p+) (prev prev))
-            (if (null? prev) e.best
-              (let ((e.0 (caar prev)) (p.0 (cdar prev)) (prev (cdr prev)))
-                (define e+ (+ e e.0))
-                (define p+ (* p p.0))
-                (if (< n p+)
-                  (loop e  p  e+     p+     prev)
-                  (loop e+ p+ e.best p.best prev)))))
-          (loop e+ p+ (+ e+ e+) (* p+ p+) (cons (cons e p) prev))))))
+    (cond ((< n 1) 0)
+          (else (define ~e (inexact->exact (truncate (log n 10))))
+                (define ~p (expt 10 ~e))
+                (cond ((< n (/ ~p 10)) (- ~e 1))
+                      ((< n    ~p    )    ~e   )
+                      (else            (+ ~e 1))))))
   (define (pr-float n.inexact)
     (define n (inexact->exact n.inexact))
     (define (finish rdigits e)
