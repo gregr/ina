374f366ffed36a803eced9926f42f84dabded2ea
gregr <greg.weiqi@gmail.com>
2022-07-16 14:36:09 -0400

    Create test-parse.rkt

---
+++ nscheme/test-parse.rkt
@@ -0,0 +1,6 @@
+#lang racket/base
+(require "nscheme.rkt" racket/include racket/match racket/splicing racket/vector)
+(include "include/base.scm")
+(include "include/syntax.scm")
+(include "include/ast.scm")
+;(include "include/parse.scm")
