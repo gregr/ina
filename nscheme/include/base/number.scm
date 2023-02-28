d8e04a7cab6486bbdda82d1fda5c768992b63484
gregr <greg.weiqi@gmail.com>
2023-02-28 17:02:55 -0500

    Define more numeric operators

--- nscheme/include/base/number.scm
+++ nscheme/include/base/number.scm
@@ -1,16 +1,29 @@
-(define (integer?! x) (has-type?! integer? 'integer? x))
+(define exact? rational?)
+(define (inexact? x) (or (f32? x) (f64? x)))
+(define (number?  x) (or (rational? x) (inexact? x)))
+
+(define (integer?!  x) (has-type?! integer?  'integer?  x))
+(define (rational?! x) (has-type?! rational? 'rational? x))
+
+(define (=  a b) (rational?! a) (rational?! b) (eqv? a b))
+(define (<= a b) (rational?! a) (rational?! b) (or (eqv? a b) (< a b)))
+(define (>= a b) (rational?! a) (rational?! b) (or (eqv? a b) (< b a)))
+(define (>  a b) (rational?! a) (rational?! b) (< b a))
 
 (define (zero?     x) (= x 0))
 (define (positive? x) (> x 0))
 (define (negative? x) (< x 0))
 
+(define (integer-floor-div a b) (let-values (((d m) (integer-floor-divmod a b))) d))
+(define (integer-floor-mod a b) (let-values (((d m) (integer-floor-divmod a b))) m))
+
 (define (even? x)
   (integer?! x)
-  (= (remainder x 2) 0))
+  (= (integer-floor-mod x 2) 0))
 
 (define (odd? x)
   (integer?! x)
-  (= (remainder x 2) 1))
+  (= (integer-floor-mod x 2) 1))
 
 (define (abs x) (if (< x 0) (- x) x))
 
@@ -29,7 +42,7 @@
     (()         0)
     ((a)        (abs a))
     ((a b . x*) (define (gcd a b)
-                  (let ((r (remainder a b)))
+                  (let ((r (integer-floor-mod a b)))
                     (cond ((< 0 r) (gcd b r))
                           (else    b))))
                 (let loop ((a (abs a)) (b (abs b)) (x* x*))
