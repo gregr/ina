ed2ba95a62b90a616f1c5ddd182d9fb5faae7298
gregr <greg.weiqi@gmail.com>
2023-06-28 21:18:48 -0400

    Remove record dependency for errors

--- nscheme/include/boot/error.scm
+++ nscheme/include/boot/error.scm
@@ -4,23 +4,18 @@
 
 (define (has-type?! type? expected value) (unless (type? value) (type-error expected value)))
 
-(splicing-local
-  ((define rtd.error (make-rtd 'error #f #f '#(detail))))
-  (define (make-error . detail*) ((record-constructor rtd.error) detail*))
-  (define error?        (record-predicate rtd.error))
-  (define error-detail* (record-field-position-accessor rtd.error 0)))
+(define (make-error . detail*) (vector 'error detail*))
+(define (error? x) (and (vector? x) (= (vector-length x) 2) (eq? (vector-ref x 0) 'error)))
+(define (error-detail* x) (vector-ref x 1))
 
 (define (make-lexical-error description location)
   (make-error 'lexical (cons 'description description) (cons 'location location)))
-
 (define (make-syntax-error description . stx*)
   (make-error 'syntax (cons 'description description) (cons 'location* stx*)))
-
 (define (make-system-error kind . detail*) (apply make-error (cons 'system kind) detail*))
 
 (define (raise-lexical-error description location)
   (raise (make-lexical-error description location)))
-
 (define (raise-syntax-error description . stx*) (raise (apply make-syntax-error description stx*)))
 (define (raise-system-error sub-kind . detail*) (raise (apply make-system-error sub-kind detail*)))
 
