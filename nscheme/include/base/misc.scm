05bb935a3ef7fe7316d5faf4490266322413a90a
gregr <greg.weiqi@gmail.com>
2024-03-07 16:49:22 -0500

    Make box definitions available earlier

--- nscheme/include/base/misc.scm
+++ nscheme/include/base/misc.scm
@@ -4,3 +4,7 @@
 (define (call-with-values produce consume) (apply/values consume (produce)))
 
 (define (error . detail*) (panic 'error detail*))
+
+(define (box        x) (let ((b (make-mvector 1 0))) (mvector-set! b 0 x) b))
+(define (unbox      b) (mvector-ref  b 0))
+(define (set-box! b x) (mvector-set! b 0 x))
