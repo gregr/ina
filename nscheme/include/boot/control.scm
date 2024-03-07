cf0dce54a2941094ae80038453910c149d579f63
gregr <greg.weiqi@gmail.com>
2024-03-06 22:40:22 -0500

    Prepare coroutine state

--- nscheme/include/boot/control.scm
+++ nscheme/include/boot/control.scm
@@ -1,19 +1,36 @@
 (splicing-local
-  ((splicing-let ((offset.dynamic-env          0)
-                  (offset.virtual-thread-state 1))
+  ((splicing-let ((offset.ntst.dynamic-env     0)
+                  (offset.ntst.coroutine-state 1)
+                  (offset.cst.controller  0)
+                  (offset.cst.current-raw 1))
      (define dynamic-env
        (case-lambda
-         (()  (mvector-ref  (native-thread-local-register) offset.dynamic-env))
-         ((x) (mvector-set! (native-thread-local-register) offset.dynamic-env x))))
-     (define virtual-thread-state
+         (()  (mvector-ref  (native-thread-local-register) offset.ntst.dynamic-env))
+         ((x) (mvector-set! (native-thread-local-register) offset.ntst.dynamic-env x))))
+     (define current-coroutine-state
        (case-lambda
-         (()  (mvector-ref  (native-thread-local-register) offset.virtual-thread-state))
-         ((x) (mvector-set! (native-thread-local-register) offset.virtual-thread-state x))))
-     (define (initialize-native-thread-local-state!)
-       (let ((dynamic-state.initial (make-mvector 2 0)))
-         (mvector-set! dynamic-state.initial offset.dynamic-env '())
-         (mvector-set! dynamic-state.initial offset.virtual-thread-state #f)
-         (native-thread-local-register dynamic-state.initial))))
+         (()  (mvector-ref  (native-thread-local-register) offset.ntst.coroutine-state))
+         ((x) (mvector-set! (native-thread-local-register) offset.ntst.coroutine-state x))))
+
+     (define (make-coroutine-state rcr)
+       (let* ((cst        (make-mvector 2 0))
+              (controller (lambda x* (with-dynamic-env-extend
+                                       (lambda () (current-coroutine-state cst)
+                                         (apply (coroutine-state-current-raw cst) x*))))))
+         (set-coroutine-state-controller!  cst controller)
+         (set-coroutine-state-current-raw! cst rcr)
+         cst))
+     (define (set-coroutine-state-controller!  st ctrl) (mvector-set! st offset.cst.controller  ctrl))
+     (define (set-coroutine-state-current-raw! st rcr)  (mvector-set! st offset.cst.current-raw rcr))
+     (define (coroutine-state-controller  st) (mvector-ref st offset.cst.controller))
+     (define (coroutine-state-current-raw st) (mvector-ref st offset.cst.current-raw))
+
+     (define (initialize-native-thread-state!)
+       (let ((native-thread-state.initial (make-mvector 2 0))
+             (cst                         (make-coroutine-state (current-raw-coroutine))))
+         (mvector-set! native-thread-state.initial offset.ntst.dynamic-env '())
+         (mvector-set! native-thread-state.initial offset.ntst.coroutine-state cst)
+         (native-thread-local-register native-thread-state.initial))))
 
    (define (with-untagged-escape-prompt on-escape thunk)
      (let ((saved-denv (dynamic-env)))
@@ -25,7 +42,7 @@
 
   ;; This call initializes dynamic-extent state slots for the current native thread.
   ;; If/when another native thread is started, it should call this initializer before continuing.
-  (initialize-native-thread-local-state!)
+  (initialize-native-thread-state!)
 
   (define (dynamic-env-ref key default-value)
     (let loop ((frame* (dynamic-env)))
@@ -84,7 +101,16 @@
       (final-thunk)
       (apply values result*)))
 
-  (define escape-to-prompt raw-escape-to-prompt))
+  (define (current-coroutine) (coroutine-state-controller (current-coroutine-state)))
+
+  (define (make-coroutine proc)
+    (let* ((rcr (make-raw-coroutine
+                  (lambda x* (with-dynamic-env-clear (lambda () (apply proc x*))))))
+           (cst (make-coroutine-state rcr)))
+      (coroutine-state-controller cst)))
+  )
+
+(define escape-to-prompt raw-escape-to-prompt)
 
 (define make-escape-prompt-tag
   (case-lambda
@@ -98,14 +124,6 @@
         (()  (dynamic-env-ref  key default-value))
         ((x) (dynamic-env-set! key x))))
     param))
-
-(splicing-local
-  ((define ((raw-coroutine->coroutine rcr) . x*)
-     (with-dynamic-env-extend (lambda () (apply rcr x*)))))
-  (define (current-coroutine) (raw-coroutine->coroutine (current-raw-coroutine)))
-  (define (make-coroutine proc)
-    (raw-coroutine->coroutine
-      (make-raw-coroutine (lambda x* (with-dynamic-env-clear (lambda () (apply proc x*))))))))
 
 ;; TODO: make-generator
 ;; - asymmetric, hierarchical coroutines
