51caa528ca1885955bfca5ef4f414e4921c6ab5d
gregr <greg.weiqi@gmail.com>
2024-09-18 09:40:57 -0400

    Protect access to raise handlers

--- nscheme/include/base/prompt.scm
+++ nscheme/include/base/prompt.scm
@@ -82,22 +82,24 @@
 ;;;;;;;;;;;;;;;;;;;;;;
 ;;; Raise handlers ;;;
 ;;;;;;;;;;;;;;;;;;;;;;
-(define current-raise-handler* (make-parameter '()))
-(define (raise-continuable x)
-  (let loop ((h* (current-raise-handler*)))
-    (unless (null? h*)
-      (current-raise-handler*
-        (cdr h*)
-        (lambda ()
-          ((car h*) x)
-          (loop (cdr h*)))))))
+
+(splicing-local
+  ((define current-raise-handler* (make-parameter '())))
+  (define (with-raise-handler handle thunk)
+    (current-raise-handler* (cons handle (current-raise-handler*)) thunk))
+  (define (with-raise-handler* handle* thunk)
+    (current-raise-handler* (append handle* (current-raise-handler*))) thunk)
+  (define (raise-continuable x)
+    (let loop ((h* (current-raise-handler*)))
+      (unless (null? h*)
+        (current-raise-handler*
+          (cdr h*)
+          (lambda ()
+            ((car h*) x)
+            (loop (cdr h*))))))))
 (define (raise          x) (raise-continuable x) (panic 'raise x))
 (define (raise/continue x) (with-restart:continue 'raise/continue (lambda () (raise x))))
 
-(define (with-raise-handler handle thunk)
-  (current-raise-handler* (cons handle (current-raise-handler*)) thunk))
-(define (with-raise-handler* handle* thunk)
-  (current-raise-handler* (append handle* (current-raise-handler*))) thunk)
 
 (define (with-raise-handler:catch catch? handle thunk)
   (with-escape
