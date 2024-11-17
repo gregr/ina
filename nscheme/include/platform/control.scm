7c4ba608c43937fbbb022caefaa90e7b98c14d7c
gregr <greg.weiqi@gmail.com>
2024-11-17 14:43:12 -0500

    Remove thread-resume-evt thread-suspend-evt

--- nscheme/include/platform/control.scm
+++ nscheme/include/platform/control.scm
@@ -2,16 +2,16 @@
   (cons
     '(
       make-parameter current-panic-handler current-custodian make-custodian custodian-shutdown-all
-      current-thread-group make-thread-group current-thread thread thread/suspend-to-kill
-      thread-resume thread-wait thread-resume-evt thread-suspend-evt thread-dead-evt
+      current-thread-group make-thread-group current-thread
+      thread thread/suspend-to-kill thread-resume thread-wait thread-dead-evt
       sync sync/default handle-evt choice-evt guard-evt nack-guard-evt replace-evt never-evt
       make-channel channel-get channel-put channel-get-evt channel-put-evt
       make-semaphore semaphore-post semaphore-wait
       current-time/type sleep-seconds-nanoseconds)
     (list
       make-parameter current-panic-handler current-custodian make-custodian custodian-shutdown-all
-      current-thread-group make-thread-group current-thread thread thread/suspend-to-kill
-      thread-resume thread-wait thread-resume-evt thread-suspend-evt thread-dead-evt
+      current-thread-group make-thread-group current-thread
+      thread thread/suspend-to-kill thread-resume thread-wait thread-dead-evt
       sync sync/default handle-evt choice-evt guard-evt nack-guard-evt replace-evt never-evt
       make-channel channel-get channel-put channel-get-evt channel-put-evt
       make-semaphore semaphore-post semaphore-wait
--- nscheme/platform/racket/nscheme.rkt
+++ nscheme/platform/racket/nscheme.rkt
@@ -29,8 +29,8 @@
   standard-input-port standard-output-port standard-error-port
 
   make-parameter current-panic-handler current-custodian make-custodian custodian-shutdown-all
-  current-thread-group make-thread-group current-thread thread thread/suspend-to-kill
-  thread-resume thread-wait thread-resume-evt thread-suspend-evt thread-dead-evt
+  current-thread-group make-thread-group current-thread
+  thread thread/suspend-to-kill thread-resume thread-wait thread-dead-evt
   sync sync/default handle-evt choice-evt guard-evt nack-guard-evt replace-evt never-evt
   make-channel channel-get channel-put channel-get-evt channel-put-evt
   make-semaphore semaphore-post semaphore-wait
