bfb43f8590c614c0a3acc48b38b0739f32f917fc
gregr <greg.weiqi@gmail.com>
2024-12-19 21:28:24 -0500

    Remove semaphore primitives entirely

--- nscheme/platform/racket/nscheme.rkt
+++ nscheme/platform/racket/nscheme.rkt
@@ -32,7 +32,6 @@
   current-thread-group make-thread-group current-thread thread thread-wait thread-dead-evt
   sync sync/default handle-evt choice-evt guard-evt nack-guard-evt replace-evt never-evt
   make-channel channel-get channel-put channel-put-evt
-  make-semaphore semaphore-post semaphore-wait
   current-time/type sleep-seconds-nanoseconds
 
   panic apply values
