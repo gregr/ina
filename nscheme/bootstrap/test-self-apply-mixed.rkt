3be31e936966afbd582b5277c979e56d343cb60a
gregr <greg.weiqi@gmail.com>
2025-02-06 17:03:38 -0500

    Remove dependencies in test-self-apply-mixed.rkt

--- nscheme/bootstrap/test-self-apply-mixed.rkt
+++ nscheme/bootstrap/test-self-apply-mixed.rkt
@@ -1,14 +1,13 @@
 #lang racket/base
-(require
-  "../platform/racket/nscheme.rkt" "include.rkt"
-  (prefix-in rkt: racket/base) (prefix-in rkt: racket/pretty))
-;(require profile)
+(require "../platform/racket/nscheme.rkt" "include.rkt")
 
 ;;; This program runs a minimal, ahead-of-time cross-compilation process on the code for an
 ;;; interactive system and its dependencies, targeting each platform.
 
 ;;; This variant of bootstrapping is unstratified, mixing values computed at different phases.  That
 ;;; means compilation must support cross-stage persistence.
+
+(define (with-time thunk) (with-milliseconds displayln thunk))
 
 (let ()
   (define stx*.test (list '(list
@@ -20,12 +19,12 @@
                                        '(1 2) '(3 4 5) '(a b c))
                             (fold-right (lambda (acc x y) (cons (cons x y) acc))
                                         '(1 2) '(3 4 5) '(a b c)))))
-  (rkt:displayln "parsing test:")
+  (displayln "parsing test:")
   ;; ~0ms
-  (define E.test (time (parse-body env.large stx*.test)))
-  (rkt:displayln "evaluating test:")
+  (define E.test (with-time (lambda () (parse-body env.large stx*.test))))
+  (displayln "evaluating test:")
   ;; ~0ms
-  (rkt:pretty-write (time (ns-eval E.test)))
+  (pretty-write (with-time (lambda () (ns-eval E.test))))
   ;==>
   ;(3
   ; 15
@@ -54,17 +53,17 @@
                                (fold-right (lambda (acc x y) (cons (cons x y) acc))
                                            '(1 2) '(3 4 5) '(a b c)))))
      (parse-body env.large stx*.test))))
-(rkt:displayln "parsing self-apply1:")
+
+
+(displayln "parsing self-apply1:")
 ;; ~2ms
-(define E.self-apply1 (time (parse-body env.large+posix+privileged stx*.self-apply1)))
-;(define E.self-apply1 (profile (parse-body env.large+posix+privileged stx*.self-apply1)))
-(rkt:displayln "evaluating self-apply1 to parse self-apply2:")
+(define E.self-apply1 (with-time (lambda () (parse-body env.large+posix+privileged stx*.self-apply1))))
+(displayln "evaluating self-apply1 to parse self-apply2:")
 ;; ~553ms
-(define E.self-apply2 (time (ns-eval E.self-apply1)))
-;(define E.self-apply2 (profile (ns-eval E.self-apply1)))
-(rkt:displayln "evaluating self-apply2:")
+(define E.self-apply2 (with-time (lambda () (ns-eval E.self-apply1))))
+(displayln "evaluating self-apply2:")
 ;; ~1ms
-(rkt:pretty-write (time (ns-eval E.self-apply2)))
+(pretty-write (with-time (lambda () (ns-eval E.self-apply2))))
 ;==>
 ;(3
 ; 15
