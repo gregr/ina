774c776ba1c8445b9c98c84b2776151208859c6d
gregr <greg.weiqi@gmail.com>
2025-02-08 09:26:40 -0500

    Move include.scm to posix/env/

--- nscheme/bootstrap/test-eval.rkt
+++ nscheme/bootstrap/test-eval.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 (require "../platform/racket/nscheme.rkt" "include.rkt" racket/include)
 (include "../include/platform/env/primitive.scm")
-(include "../include/platform/posix/include.scm")
+(include "../include/platform/posix/env/include.scm")
 (include "../include/platform/env/evaluated.scm")
 (include "../include/platform/posix/env/primitive.scm")
 (include "../include/platform/posix/env/evaluated.scm")
