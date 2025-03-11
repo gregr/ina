160396771a6c6f12531d2f4ff830e07c57631c15
gregr <greg.weiqi@gmail.com>
2025-03-11 14:17:18 -0400

    Curry write operations that receive notate

--- nscheme/include/base/io.scm
+++ nscheme/include/base/io.scm
@@ -5,28 +5,21 @@
       ((port) (go port)))))
 
 (splicing-local
-  ((define (make-write printer->layout newline?)
+  ((define ((make-write/notate printer->layout newline?) notate)
      (define (go x port) (let ((p (printer:port port)))
                            (notate x (writer:layout (printer->layout p)))
                            (when newline? (printer-newline p))))
      (case-lambda
        ((x)      (go x (current-output-port)))
-       ((x port) (go x port))))
-   (define (make-write/notate printer->layout newline?)
-     (define (go notate x port) (let ((p (printer:port port)))
-                                  (notate x (writer:layout (printer->layout p)))
-                                  (when newline? (printer-newline p))))
-     (case-lambda
-       ((notate x)      (go notate x (current-output-port)))
-       ((notate x port) (go notate x port)))))
-  (define write                (make-write        layout:single-line                 #f))
-  (define writeln              (make-write        layout:single-line                 #t))
-  (define pretty-write         (make-write        (lambda (p) (layout:pretty  p 78)) #t))
-  (define compact-write        (make-write        (lambda (p) (layout:compact p 78)) #t))
+       ((x port) (go x port)))))
   (define write/notate         (make-write/notate layout:single-line                 #f))
   (define writeln/notate       (make-write/notate layout:single-line                 #t))
   (define pretty-write/notate  (make-write/notate (lambda (p) (layout:pretty  p 78)) #t))
-  (define compact-write/notate (make-write/notate (lambda (p) (layout:compact p 78)) #t)))
+  (define compact-write/notate (make-write/notate (lambda (p) (layout:compact p 78)) #t))
+  (define write                (write/notate         notate))
+  (define writeln              (writeln/notate       notate))
+  (define pretty-write         (pretty-write/notate  notate))
+  (define compact-write        (compact-write/notate notate)))
 
 (define display
   (let ((go (lambda (x port)
