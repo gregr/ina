93ad00032401aa645ee0066fc6a2021033ab56bf
gregr <greg.weiqi@gmail.com>
2025-05-26 14:00:25 -0400

    Hide microKanren definitions for formula parsing

--- nscheme/test/mk/formula-parsers.scm
+++ nscheme/test/mk/formula-parsers.scm
@@ -1,195 +1,196 @@
 (begin-meta
-  ;;;;;;;;;;;;;;;;;;;
-  ;;; microKanren ;;;
-  ;;;;;;;;;;;;;;;;;;;
-  (define (var   id)  (vector id))
-  (define (var?  x)   (vector? x))
-  (define (var=? a b) (eqv? (vector-ref a 0) (vector-ref b 0)))
-  (define var.initial (var -1))
+  (splicing-local
+    (;;;;;;;;;;;;;;;;;;;
+     ;;; microKanren ;;;
+     ;;;;;;;;;;;;;;;;;;;
+     (define (var   id)  (vector id))
+     (define (var?  x)   (vector? x))
+     (define (var=? a b) (eqv? (vector-ref a 0) (vector-ref b 0)))
+     (define var.initial (var -1))
 
-  (define sub.empty '())
+     (define sub.empty '())
 
-  (define (walk t sub)
-    (let ((xt (and (var? t) (assp (lambda (x) (var=? t x)) sub))))
-      (if xt (walk (cdr xt) sub) t)))
+     (define (walk t sub)
+       (let ((xt (and (var? t) (assp (lambda (x) (var=? t x)) sub))))
+         (if xt (walk (cdr xt) sub) t)))
 
-  (define (walk* t sub)
-    (let ((t (walk t sub)))
-      (if (pair? t)
-          (cons (walk* (car t) sub) (walk* (cdr t) sub))
-          t)))
+     (define (walk* t sub)
+       (let ((t (walk t sub)))
+         (if (pair? t)
+             (cons (walk* (car t) sub) (walk* (cdr t) sub))
+             t)))
 
-  (define (occurs? x t sub)
-    (let loop ((t t))
-      (cond ((pair? t) (or (loop (walk (car t) sub))
-                           (loop (walk (cdr t) sub))))
-            ((var?  t) (var=? x t))
-            (else      #f))))
+     (define (occurs? x t sub)
+       (let loop ((t t))
+         (cond ((pair? t) (or (loop (walk (car t) sub))
+                              (loop (walk (cdr t) sub))))
+               ((var?  t) (var=? x t))
+               (else      #f))))
 
-  (define (extend-sub x t sub)
-    (and (not (occurs? x t sub)) `((,x . ,t) . ,sub)))
+     (define (extend-sub x t sub)
+       (and (not (occurs? x t sub)) `((,x . ,t) . ,sub)))
 
-  (define (make-state sub id.next)   (cons sub id.next))
-  (define (state-sub         st)     (car st))
-  (define (state-next-id     st)     (cdr st))
-  (define (set-state-next-id st id)  (make-state (state-sub st) id))
-  (define (set-state-sub     st sub) (make-state sub (state-next-id st)))
+     (define (make-state sub id.next)   (cons sub id.next))
+     (define (state-sub         st)     (car st))
+     (define (state-next-id     st)     (cdr st))
+     (define (set-state-next-id st id)  (make-state (state-sub st) id))
+     (define (set-state-sub     st sub) (make-state sub (state-next-id st)))
 
-  (define state.empty '(() . 0))
+     (define state.empty '(() . 0))
 
-  (define (call/fresh var->g)
-    (lambda (st)
-      (let ((id (state-next-id st)))
-        ((var->g (var id)) (set-state-next-id st (+ id 1))))))
+     (define (call/fresh var->g)
+       (lambda (st)
+         (let ((id (state-next-id st)))
+           ((var->g (var id)) (set-state-next-id st (+ id 1))))))
 
-  (define (assign x t st)
-    (let ((sub.new (extend-sub x t (state-sub st))))
-      (and sub.new (set-state-sub st sub.new))))
+     (define (assign x t st)
+       (let ((sub.new (extend-sub x t (state-sub st))))
+         (and sub.new (set-state-sub st sub.new))))
 
-  (define (unify u v st)
-    (let* ((sub (state-sub st))
-           (u   (walk u sub))
-           (v   (walk v sub)))
-      ;; TODO: it might also be useful to allow any procedure values to unify.
-      (cond ((eqv? u v) st)
-            ((var? u)   (if (and (var? v) (var=? u v)) st (assign u v st)))
-            ((var? v)   (assign v u st))
-            ((pair? u)  (and (pair? v) (let ((st (unify (car u) (car v) st)))
-                                         (and st (unify (cdr u) (cdr v) st)))))
-            (else       #f))))
+     (define (unify u v st)
+       (let* ((sub (state-sub st))
+              (u   (walk u sub))
+              (v   (walk v sub)))
+         ;; TODO: it might also be useful to allow any procedure values to unify.
+         (cond ((eqv? u v) st)
+               ((var? u)   (if (and (var? v) (var=? u v)) st (assign u v st)))
+               ((var? v)   (assign v u st))
+               ((pair? u)  (and (pair? v) (let ((st (unify (car u) (car v) st)))
+                                            (and st (unify (cdr u) (cdr v) st)))))
+               (else       #f))))
 
-  (define (state->stream st) (and st (cons st #f)))
+     (define (state->stream st) (and st (cons st #f)))
 
-  (define (mature? s) (or (not s) (pair? s)))
-  (define (mature  s) (if (mature? s) s (mature (s))))
-  (define (step    s) (if (mature? s) s (s)))
+     (define (mature? s) (or (not s) (pair? s)))
+     (define (mature  s) (if (mature? s) s (mature (s))))
+     (define (step    s) (if (mature? s) s (s)))
 
-  (define (s-take n s)
-    (cond ((eqv? 0 n) '())
-          (else (let ((s (mature s)))
-                  (cond ((pair? s) (cons (car s) (s-take (and n (- n 1)) (cdr s))))
-                        (else      '()))))))
+     (define (s-take n s)
+       (cond ((eqv? 0 n) '())
+             (else (let ((s (mature s)))
+                     (cond ((pair? s) (cons (car s) (s-take (and n (- n 1)) (cdr s))))
+                           (else      '()))))))
 
-  (define (pause st g) (lambda () (g st)))
+     (define (pause st g) (lambda () (g st)))
 
-  (define (mplus s1 s2)
-    (let ((s1 (step s1)))
-      (cond ((not   s1) s2)
-            ((pair? s1) (cons (car s1) (lambda () (mplus s2 (cdr s1)))))
-            (else       (lambda () (mplus s2 s1))))))
+     (define (mplus s1 s2)
+       (let ((s1 (step s1)))
+         (cond ((not   s1) s2)
+               ((pair? s1) (cons (car s1) (lambda () (mplus s2 (cdr s1)))))
+               (else       (lambda () (mplus s2 s1))))))
 
-  (define (bind s g)
-    (let ((s (step s)))
-      (cond ((not   s) #f)
-            ((pair? s) (mplus (pause (car s) g)
-                              (lambda () (bind (cdr s) g))))
-            (else      (lambda () (bind s g))))))
+     (define (bind s g)
+       (let ((s (step s)))
+         (cond ((not   s) #f)
+               ((pair? s) (mplus (pause (car s) g)
+                                 (lambda () (bind (cdr s) g))))
+               (else      (lambda () (bind s g))))))
 
-  (define (disj   g1 g2) (lambda (st) (mplus (pause st g1) (pause st g2))))
-  (define (conj   g1 g2) (lambda (st) (bind (pause st g1) g2)))
-  (define (relate thunk) (lambda (st) (pause st (thunk))))
-  (define (==     u v)   (lambda (st) (state->stream (unify u v st))))
+     (define (disj   g1 g2) (lambda (st) (mplus (pause st g1) (pause st g2))))
+     (define (conj   g1 g2) (lambda (st) (bind (pause st g1) g2)))
+     (define (relate thunk) (lambda (st) (pause st (thunk))))
+     (define (==     u v)   (lambda (st) (state->stream (unify u v st))))
 
-  ;; TODO: improve reification
-  (define (reify t st) (walk* t (state-sub st)))
+     ;; TODO: improve reification
+     (define (reify t st) (walk* t (state-sub st))))
 
-  ;;;;;;;;;;;;;;;;;;
-  ;;; miniKanren ;;;
-  ;;;;;;;;;;;;;;;;;;
-  (define vocab.formula 'formula)
-  (define (parse-formula* env stx*) (map (lambda (stx) (parse-formula env stx)) stx*))
-  (define (parse-formula env stx)
-    (define (^default) (raise-parse-error (list vocab.formula "not a formula") stx))
-    ($source (((vocabulary-parser vocab.formula ^default) env stx) env stx) stx))
-  (splicing-let (($$==         ($quote ==))
-                 ($$disj       ($quote disj))
-                 ($$conj       ($quote conj))
-                 ($$pause      ($quote pause))
-                 ($$call/fresh ($quote call/fresh))
-                 ($$s-take     ($quote s-take))
-                 ($$reify      ($quote reify))
-                 ($$map        ($quote map)))
-    (define ($==         a b)  ($call $$==         a b))
-    (define ($disj       a b)  ($call $$disj       a b))
-    (define ($conj       a b)  ($call $$conj       a b))
-    (define ($pause      a b)  ($call $$pause      a b))
-    (define ($call/fresh x->g) ($call $$call/fresh x->g))
-    (define ($s-take     n s)  ($call $$s-take     n s))
-    (define ($reify      x st) ($call $$reify      x st))
-    (define ($map        f x*) ($call $$map        f x*)))
-  (define $var.initial ($quote var.initial))
-  (define $state.empty ($quote state.empty))
+    ;;;;;;;;;;;;;;;;;;
+    ;;; miniKanren ;;;
+    ;;;;;;;;;;;;;;;;;;
+    (define vocab.formula 'formula)
+    (define (parse-formula* env stx*) (map (lambda (stx) (parse-formula env stx)) stx*))
+    (define (parse-formula env stx)
+      (define (^default) (raise-parse-error (list vocab.formula "not a formula") stx))
+      ($source (((vocabulary-parser vocab.formula ^default) env stx) env stx) stx))
+    (splicing-let (($$==         ($quote ==))
+                   ($$disj       ($quote disj))
+                   ($$conj       ($quote conj))
+                   ($$pause      ($quote pause))
+                   ($$call/fresh ($quote call/fresh))
+                   ($$s-take     ($quote s-take))
+                   ($$reify      ($quote reify))
+                   ($$map        ($quote map)))
+      (define ($==         a b)  ($call $$==         a b))
+      (define ($disj       a b)  ($call $$disj       a b))
+      (define ($conj       a b)  ($call $$conj       a b))
+      (define ($pause      a b)  ($call $$pause      a b))
+      (define ($call/fresh x->g) ($call $$call/fresh x->g))
+      (define ($s-take     n s)  ($call $$s-take     n s))
+      (define ($reify      x st) ($call $$reify      x st))
+      (define ($map        f x*) ($call $$map        f x*)))
+    (define $var.initial ($quote var.initial))
+    (define $state.empty ($quote state.empty))
 
-  (define ($run env $count param ^body)
-    ($map ($lambda '(st) (lambda ($st) ($reify $var.initial $st)))
-          ($s-take $count
-                   ($pause $state.empty
-                           (if (identifier? param)
-                               ($let/env env (list param) (list $var.initial) ^body)
-                               (let ((param* (syntax->list param)))
-                                 (parse-param* param*)
-                                 ($fresh env param*
-                                         (lambda (env)
-                                           (let (($x* (parse-expression* env param*)))
-                                             ($conj ($== $var.initial (apply $list $x*))
-                                                    (^body env)))))))))))
+    (define ($run env $count param ^body)
+      ($map ($lambda '(st) (lambda ($st) ($reify $var.initial $st)))
+            ($s-take $count
+                     ($pause $state.empty
+                             (if (identifier? param)
+                                 ($let/env env (list param) (list $var.initial) ^body)
+                                 (let ((param* (syntax->list param)))
+                                   (parse-param* param*)
+                                   ($fresh env param*
+                                           (lambda (env)
+                                             (let (($x* (parse-expression* env param*)))
+                                               ($conj ($== $var.initial (apply $list $x*))
+                                                      (^body env)))))))))))
 
-  (define ($fresh env param* ^body)
-    (let loop ((env env) (param* param*))
-      (if (null? param*)
-          (^body env)
-          ($call/fresh ($lambda/env env (list (car param*))
-                                    (lambda (env) (loop env (cdr param*))))))))
+    (define ($fresh env param* ^body)
+      (let loop ((env env) (param* param*))
+        (if (null? param*)
+            (^body env)
+            ($call/fresh ($lambda/env env (list (car param*))
+                                      (lambda (env) (loop env (cdr param*))))))))
 
-  (define ($conj* $g*)
-    (if (null? $g*)
-        ($== ($quote #t) ($quote #t))
-        (let loop (($g (car $g*)) ($g* (cdr $g*)))
-          (if (null? $g*) $g (loop ($conj $g (car $g*)) (cdr $g*))))))
+    (define ($conj* $g*)
+      (if (null? $g*)
+          ($== ($quote #t) ($quote #t))
+          (let loop (($g (car $g*)) ($g* (cdr $g*)))
+            (if (null? $g*) $g (loop ($conj $g (car $g*)) (cdr $g*))))))
 
-  (define ($disj* $g*)
-    (if (null? $g*)
-        ($== ($quote #t) ($quote #f))
-        (let loop (($g (car $g*)) ($g* (cdr $g*)))
-          (if (null? $g*) $g ($disj $g (loop (car $g*) (cdr $g*)))))))
+    (define ($disj* $g*)
+      (if (null? $g*)
+          ($== ($quote #t) ($quote #f))
+          (let loop (($g (car $g*)) ($g* (cdr $g*)))
+            (if (null? $g*) $g ($disj $g (loop (car $g*) (cdr $g*)))))))
 
-  (define (parse-run* env param . fm*) (apply parse-run env #f param fm*))
+    (define (parse-run* env param . fm*) (apply parse-run env #f param fm*))
 
-  (define (parse-run env count param . fm*)
-    ($run env (parse-expression env count) param (lambda (env) ($conj* (parse-formula* env fm*)))))
+    (define (parse-run env count param . fm*)
+      ($run env (parse-expression env count) param (lambda (env) ($conj* (parse-formula* env fm*)))))
 
-  (define (parse-conde env . stx*.fm*)
-    ($disj* (map (lambda (stx.fm*) ($conj* (parse-formula* env (syntax->list stx.fm*))))
-                 stx*.fm*)))
+    (define (parse-conde env . stx*.fm*)
+      ($disj* (map (lambda (stx.fm*) ($conj* (parse-formula* env (syntax->list stx.fm*))))
+                   stx*.fm*)))
 
-  (define (parse-fresh env param* . fm*)
-    (let ((param* (syntax->list param*)))
-      (parse-param* param*)
-      ($fresh env param* (lambda (env) ($conj* (parse-formula* env fm*))))))
-
-  (define (parse-== env lhs rhs) ($== (parse-expression env lhs) (parse-expression env rhs)))
-
-  (define (parse-define-relation env.d env stx.rhead . fm*)
-    (let ((rhead (syntax->list stx.rhead)))
-      (when (null? rhead) (raise-parse-error "empty relation head" stx.rhead))
-      (let ((rid (car rhead)) (param* (cdr rhead)))
-        (parse-identifier rid)
+    (define (parse-fresh env param* . fm*)
+      (let ((param* (syntax->list param*)))
         (parse-param* param*)
-        (($d:define/vocabulary
-           vocab.formula
-           (lambda ($rel)
-             (lambda (env stx)
-               (let ((arg* (cdr (syntax->list stx))))
-                 (unless (= (length param*) (length arg*))
-                   (raise-parse-error
-                     (list "relation arity mismatch"
-                           'relation rhead 'given (length arg*) 'expected (length param*))
-                     stx))
-                 ($call* $rel (parse-expression* env arg*))))))
-         env.d rid
-         (let ((env (env-read-only env)))
-           (lambda () ($lambda/env env param* (lambda (env)
-                                                ($conj* (parse-formula* env fm*)))))))))))
+        ($fresh env param* (lambda (env) ($conj* (parse-formula* env fm*))))))
+
+    (define (parse-== env lhs rhs) ($== (parse-expression env lhs) (parse-expression env rhs)))
+
+    (define (parse-define-relation env.d env stx.rhead . fm*)
+      (let ((rhead (syntax->list stx.rhead)))
+        (when (null? rhead) (raise-parse-error "empty relation head" stx.rhead))
+        (let ((rid (car rhead)) (param* (cdr rhead)))
+          (parse-identifier rid)
+          (parse-param* param*)
+          (($d:define/vocabulary
+             vocab.formula
+             (lambda ($rel)
+               (lambda (env stx)
+                 (let ((arg* (cdr (syntax->list stx))))
+                   (unless (= (length param*) (length arg*))
+                     (raise-parse-error
+                       (list "relation arity mismatch"
+                             'relation rhead 'given (length arg*) 'expected (length param*))
+                       stx))
+                   ($call* $rel (parse-expression* env arg*))))))
+           env.d rid
+           (let ((env (env-read-only env)))
+             (lambda () ($lambda/env env param* (lambda (env)
+                                                  ($conj* (parse-formula* env fm*))))))))))))
 
 (define-vocabulary define-relation
   vocab.definition (operator-parser parse-define-relation 2 #f))
@@ -203,7 +204,7 @@
   vocab.formula (operator-parser parse-fresh 2 #f))
 (define-vocabulary conde
   vocab.formula (operator-parser parse-conde 1 #f))
-(add-vocabulary! ==
+(define-vocabulary ==
   vocab.formula (operator-parser parse-== 2 2))
 
 (define-vocabulary test
