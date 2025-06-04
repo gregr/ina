d36dca050e8df07705a77a9535f6bd3ccae0d8be
gregr <greg.weiqi@gmail.com>
2025-06-04 15:32:24 -0400

    Define define-vocabulary-syntax-binder

--- nscheme/src/extended/meta.scm
+++ nscheme/src/extended/meta.scm
@@ -19,3 +19,28 @@
     (parse-quasiquote-X vocab.quasiquote-syntax 'quasiquote-syntax 'unsyntax 'unsyntax-splicing
                         parse-quote-syntax)
     1 1))
+
+(begin-meta
+  (define ((macro-parser/parse introduce-definitions? parse) env.op op)
+    (let ((transcribe (syntax-transcribe/parse introduce-definitions? parse)))
+      (lambda (env.use stx) (transcribe stx op env.op env.use)))))
+
+(define-syntax (define-vocabulary-syntax-binder stx)
+  (match (syntax->list stx)
+    ((list _ name bind-in-vocabulary vocabulary-name introduce-definitions? vocabulary-parser)
+     (quasiquote-syntax
+       (define-syntax (#,name stx)
+         (match (syntax->list stx)
+           ((cons* _ lhs rhs*)
+            (let loop ((lhs lhs) (rhs* rhs*))
+              (if (identifier? lhs)
+                  (begin
+                    (unless (null? (cdr rhs*)) (raise-parse-error "too many right-hand-side expressions" stx))
+                    (quasiquote-syntax
+                      (#,(quote-syntax #,bind-in-vocabulary) #,lhs
+                       #,(quote-syntax #,vocabulary-name)
+                       ((macro-parser/parse #,(quote-syntax #,introduce-definitions?)
+                                            #,(quote-syntax #,vocabulary-parser))
+                        (current-environment) #,(car rhs*)))))
+                  (match (syntax->list lhs)
+                    ((cons lhs param) (loop lhs (list (quasiquote-syntax (lambda #,param . #,rhs*)))))))))))))))
