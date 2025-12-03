2cf975557816e420ba4546af4ab98078fa7c5904
gregr <greg.weiqi@gmail.com>
2025-12-03 10:05:58 -0500

    Improve LLL C types

--- nscheme/src/compiler/lll-c.scm
+++ nscheme/src/compiler/lll-c.scm
@@ -1,22 +1,34 @@
 ;;;;;;;;;;;;;;;;;
 ;;; LLL for C ;;;
 ;;;;;;;;;;;;;;;;;
+(define LLL-C-types
+  ##eos"typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long s64;
+"eos##)
+
 (define (LLL-emit-C P)
+  (define u64-suffix "ull")
   (define emit (let ((out (current-output-port))) (lambda (line) (display line out))))
   (define Label? string?)
   (define (Location x) (if (symbol? x)
                            (symbol->string x)
                            (and (pair? x) (eqv? (car x) 'memory)
                                 (let ((type (case (cadr x)
-                                              ((1) "unsigned char")
-                                              ((2) "unsigned short")
-                                              ((4) "unsigned int")
-                                              (else "long"))))
+                                              ((1) "u8")
+                                              ((2) "u16")
+                                              ((4) "u32")
+                                              (else "u64"))))
                                   (string-append "*(" type "*)(" (Expr (caddr x)) ")")))))
   (define (Expr x)
     (define (Subexpr x) (if (pair? x) (string-append "(" (Expr x) ")") (Expr x)))
     (cond ((Location x))
-          ((integer? x) (string-append (number->string x) "L"))
+          ((integer? x) (string-append (number->string x) u64-suffix))
           (else (string-append (Subexpr (cadr x)) " " (symbol->string (car x)) " "
                                (Subexpr (caddr x))))))
   (let loop ((S P))
@@ -25,7 +37,7 @@
         (apply (case (car S)
                  ((set!) (lambda (lhs rhs)
                            (emit (if (Label? rhs)
-                                     (string-append (Location lhs) " = (long*)&&" rhs ";\n")
+                                     (string-append (Location lhs) " = (u64*)&&" rhs ";\n")
                                      (string-append (Location lhs) " = " (Expr rhs) ";\n")))))
                  ((jump) (lambda (x) (emit (if (Label? x)
                                                (string-append "goto " x ";\n")
