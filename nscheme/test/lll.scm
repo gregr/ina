2c438315f4af2eef516aef2c2c5d6d451f8df7c7
gregr <greg.weiqi@gmail.com>
2025-12-02 22:01:55 -0500

    Support LLL labels and jumps

--- nscheme/test/lll.scm
+++ nscheme/test/lll.scm
@@ -3,27 +3,7 @@
 (include "../src/compiler/lll-c.scm")
 (include "../src/compiler/lll-x86-64.scm")
 
-(let ((P '(begin
-            (set! rax 55)
-            (begin
-              (set! rdx 11)
-              (set! rax (+ rax 7)))
-            (set! rax (* rax rdx))
-            (begin
-              (set! r8 #x12345678)
-              (set! r9 #x9ABCDEF0)
-              (set! r10 #x1000)
-              (set! r11 -16)
-              (set! (memory 8 (+ r10 -32)) r8)
-              (set! (memory 8 r10) r8)
-              (set! (memory 4 (+ r10 4)) r9)
-              (set! (memory 8 (+ r10 r11)) r9)
-              (set! r8 (memory 2 (+ r10 4)))
-              (set! r9 (memory 8 r10))
-              (set! r8 (* r8 2))
-              (set! r9 (* r9 2))
-              (set! rdi (memory 8 (+ r10 r11)))
-              (set! rsi (memory 8 (+ r10 -32)))))))
+(define (LLL-test P)
   (displayln "LLL:")
   (pretty-write P)
   (LLL-validate P)
@@ -35,4 +15,48 @@
   (newline)
   (displayln "LLL x86-64:")
   (LLL-validate-x86-64 P)
-  (LLL-emit-x86-64-at&t P))
+  (LLL-emit-x86-64-at&t P)
+  (newline))
+
+(for-each
+  LLL-test
+  '((begin
+      (set! rax 55)
+      (begin
+        (set! rdx 11)
+        (set! rax (+ rax 7)))
+      (set! rax (* rax rdx))
+      (begin
+        (set! r8 #x12345678)
+        (set! r9 #x9ABCDEF0)
+        (set! r10 #x1000)
+        (set! r11 -16)
+        (set! (memory 8 (+ r10 -32)) r8)
+        (set! (memory 8 r10) r8)
+        (set! (memory 4 (+ r10 4)) r9)
+        (set! (memory 8 (+ r10 r11)) r9)
+        (set! r8 (memory 2 (+ r10 4)))
+        (set! r9 (memory 8 r10))
+        (set! r8 (* r8 2))
+        (set! r9 (* r9 2))
+        (set! rdi (memory 8 (+ r10 r11)))
+        (set! rsi (memory 8 (+ r10 -32)))))
+    (begin
+      (set! rax 7)
+      "start"
+      (set! rdx 8)
+      (jump "end")
+      "middle"
+      (set! rax (+ rax rdx))
+      "end"
+      (set! rdx (+ rdx rax)))
+    (begin
+      (set! r15 "end")
+      (set! rax 7)
+      "start"
+      (set! rdx 8)
+      (jump r15)
+      "middle"
+      (set! rax (+ rax rdx))
+      "end"
+      (set! rdx (+ rdx rax)))))
